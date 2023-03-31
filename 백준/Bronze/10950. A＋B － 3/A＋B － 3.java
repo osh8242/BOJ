799c4d3655323d8a0d946f7c93c2786e14bc2d4e
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-03-31 21:07:42 +0900

    [Bronze V] Title: A+B - 3, Time: 88 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(a + b);
+		}
+
+	}
+
+}
