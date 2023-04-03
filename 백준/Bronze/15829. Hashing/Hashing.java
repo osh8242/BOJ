feef928cce0c8b5773ee574179768af5d38662cd
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-04-03 22:07:08 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,12 +8,23 @@ public class Main {
 		char[] ch = br.readLine().toCharArray();
 		long answer = 0;
 		for (int i = 0; i < n; i++) {
-			answer += (ch[i] - 96) * (long) (Math.pow(31, i));
+			answer += (ch[i] - 96) * exp(i);
 			if (answer > 1234567891) {
 				answer %= 1234567891;
 			}
 		}
 		System.out.println(answer);
+	}
+
+	public static long exp(int i) {
+		long answer = 1;
+		for (int n = 0; n < i; n++) {
+			answer *= 31;
+			if (answer > 1234567891) {
+				answer %= 1234567891;
+			}
+		}
+		return answer;
 	}
 
 }
