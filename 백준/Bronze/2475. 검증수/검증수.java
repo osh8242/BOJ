566052ba31279479fadc255bac86513181b6ce1c
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-04-13 00:11:56 +0900

    [Bronze V] Title: 검증수, Time: 76 ms, Memory: 11392 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,18 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int residue = 0;
-		for (int i = 0; i < 5; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());		
+		int sum = 0; 
+		for (int i = 1 ; i <= 5 ; i++) {
 			int temp = Integer.parseInt(st.nextToken());
-			residue += temp * temp % 10;
+			sum += temp*temp;
 		}
-		System.out.println(residue % 10);
+		System.out.println(sum%10);
 
-	}
+	} // Main(){}	
 
-}
+
+} //Class{}
+
