59f78d836c5d1ab6bfe45717b12146ccb5586257
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-04-15 12:23:36 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 360 ms, Memory: 20628 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b));
+
+	} // Main(){}	
+
+} //Class{}
+
