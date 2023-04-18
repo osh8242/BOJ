0df24c461039bf27d3a2992f12a388d8a8a409bb
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-04-18 21:06:34 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 80 ms, Memory: 11796 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] pocket = new int[n+1];
+        for (int i = 1 ; i <= n ; i++){
+            pocket[i] = i;
+        }
+        for (int i = 1 ; i <= m ; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int[] temp = new int[b-a+1];
+            for (int j = 0 ; j < b-a+1 ; j++){
+                temp[j] = pocket[b-j];
+            }
+            for(int j = a ; j <= b ; j++){
+                pocket[j] = temp[j-a];
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1 ; i <= n ; i++ ){
+            sb.append(pocket[i]+" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
