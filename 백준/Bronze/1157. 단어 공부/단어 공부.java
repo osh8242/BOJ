32085e074fd8e568f217062f822591346bf7e65c
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-04-27 01:11:14 +0900

    [Bronze I] Title: 단어 공부, Time: 284 ms, Memory: 40220 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,39 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		HashMap<Character, Integer> count = new HashMap<>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine().toUpperCase();
-		for (int i = 0; i < str.length(); i++) {
-			if (!count.containsKey(str.charAt(i))) {
-				count.put(str.charAt(i), 1);
-			} else {
-				count.put(str.charAt(i), count.get(str.charAt(i)) + 1);
-			}
-		}
-		int max = 0;
-		boolean isDuplicate = false;
-		char ch = ' ';
-		for (char c : count.keySet()) {
-			if (count.get(c) < max) {
-				continue;
-			} else if (count.get(c) > max) {
-				ch = c;
-				max = count.get(c);
-				isDuplicate = false;
-			} else if (count.get(c) == max) {
-				isDuplicate = true;
-			}
-		}
-		if (isDuplicate) {
-			System.out.println("?");
-		} else {
-			System.out.println(ch);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().trim().toUpperCase();
+        HashMap<Character, Integer> map = new HashMap<>();
 
-	}
-
+        for (int i = 0; i < str.length(); i++) {
+            int n = 0;
+            if (map.containsKey(str.charAt(i))) {
+                n = map.get(str.charAt(i));
+            }
+            n++;
+            map.put(str.charAt(i), n);
+        }
+        int max = 0;
+        boolean isDuplicate = false;
+        char ch = ' ';
+        for (char c : map.keySet()) {
+            if (map.get(c) < max) {
+                continue;
+            } else if (map.get(c) > max) {
+                ch = c;
+                max = map.get(c);
+                isDuplicate = false;
+            } else if (map.get(c) == max) {
+                isDuplicate = true;
+            }
+        }
+        if (isDuplicate) {
+            System.out.println("?");
+        } else {
+            System.out.println(ch);
+        }
+    }
 }
