bcbfe557a6da83c223b3cc75449ca156f8a2f97e
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-05-06 20:21:19 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2220 ms, Memory: 440092 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        int T = Integer.parseInt(br.readLine());
+        for(int test = 1 ; test <= T ; test++){
+            int lines = Integer.parseInt(br.readLine());
+            for(int line = 1 ; line <= lines ; line++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int command = st.nextToken().charAt(0);
+                int num = Integer.parseInt(st.nextToken());
+                switch (command){
+                    case 'I' : {
+                        int n = 1;
+                        if(tm.containsKey(num)) n = tm.get(num)+1;
+                        tm.put(num, n);
+                        break;
+                    }
+                    case 'D' : {
+                        if(tm.size() == 0) break;
+                        switch (num){
+                            case 1 : {    
+                                if((int)tm.get(tm.lastKey()) == 1) tm.remove(tm.lastKey());
+                                else tm.put(tm.lastKey(), (int)tm.get(tm.lastKey())-1);
+                                break;
+                            }
+                            case -1 : {
+                                if ((int)tm.get(tm.firstKey()) == 1) tm.remove(tm.firstKey());
+                                else tm.put(tm.firstKey(), (int)tm.get(tm.firstKey()) - 1);
+                                break;
+                            }
+                        }
+                    } break;
+                }
+            }
+            if(tm.size() == 0) sb.append("EMPTY\n");
+            else sb.append((int)tm.lastKey() + " " + (int)tm.firstKey() + "\n");
+            tm.clear();
+        }
+        System.out.println(sb.toString());
+    } // void main()
+} //Class Main
