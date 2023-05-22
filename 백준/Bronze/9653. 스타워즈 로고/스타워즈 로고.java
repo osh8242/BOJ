007840e6a3249b7504264867b75c562c25402f11
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-05-22 11:58:25 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 120 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,17 @@
+class Main
+{
+    public static void main(String args[]) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("    8888888888  888    88888\n");
+        sb.append("   88     88   88 88   88  88\n");
+        sb.append("    8888  88  88   88  88888\n");
+        sb.append("       88 88 888888888 88   88\n");
+        sb.append("88888888  88 88     88 88    888888\n\n");
+        sb.append("88  88  88   888    88888    888888\n");
+        sb.append("88  88  88  88 88   88  88  88\n");
+        sb.append("88 8888 88 88   88  88888    8888\n");
+        sb.append(" 888  888 888888888 88  88      88\n");
+        sb.append("  88  88  88     88 88   88888888");
+        System.out.println(sb);
+    }
+}
