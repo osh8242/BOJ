e86e87bcc51e51a9158c3b009fbd8b9a1f45586b
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-05-30 09:05:23 +0900

    [Bronze V] Title: 콜센터, Time: 124 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        System.out.println(sb);
+
+    }
+
+}
