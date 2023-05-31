05693c488179b16f19e5362280566cbe04517520
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-05-31 10:13:14 +0900

    [Bronze V] Title: 웰컴, Time: 112 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+
+    }
+
+}
