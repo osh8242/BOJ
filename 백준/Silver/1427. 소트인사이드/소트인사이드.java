b579d644de5ceeb11ddcafa538d70fb0a43cd90d
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-06-09 09:10:49 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder(new String(arr));
+        System.out.println(sb.reverse());
+    }//main(){}
+
+}//class Main{}
