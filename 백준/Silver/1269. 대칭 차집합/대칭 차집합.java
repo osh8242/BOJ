593378b8f017f4e2d2a36487c3d09aba8ffb88ef
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-06-12 00:36:34 +0900

    [Silver IV] Title: 대칭 차집합, Time: 848 ms, Memory: 80416 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
@@ -11,15 +13,15 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        TreeSet<Integer> ts = new TreeSet<>();
+        Set<Integer> hs = new HashSet<>();
         st = new StringTokenizer(br.readLine());
         while(st.hasMoreTokens()){
-            ts.add(Integer.parseInt(st.nextToken()));
+            hs.add(Integer.parseInt(st.nextToken()));
         }
         int count = 0;
         st = new StringTokenizer(br.readLine());
         while(st.hasMoreTokens()){
-            if(!ts.add(Integer.parseInt(st.nextToken()))) count++;
+            if(!hs.add(Integer.parseInt(st.nextToken()))) count++;
         }
         System.out.println(n+m-count*2);
 
