a2bcd44188887702d9607ddf060175b015c87b57
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-06-11 11:44:00 +0900

    [Silver V] Title: 숫자 카드, Time: 764 ms, Memory: 117124 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 class Main {
 
@@ -10,15 +9,15 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        TreeSet<Integer> ts = new TreeSet<>();
+        boolean[] hasCard = new boolean[20000001];
         StringTokenizer st = new StringTokenizer(br.readLine());
         while(n-->0){
-            ts.add(Integer.parseInt(st.nextToken()));
+            hasCard[Integer.parseInt(st.nextToken())+10000000] = true;
         }
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         while(m-->0){
-            if(ts.contains(Integer.parseInt(st.nextToken()))) sb.append("1 ");
+            if(hasCard[Integer.parseInt(st.nextToken())+10000000]) sb.append("1 ");
             else sb.append("0 ");
         }
         System.out.println(sb);
