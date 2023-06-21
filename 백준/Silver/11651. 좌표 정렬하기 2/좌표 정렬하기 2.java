0b132632c7aa09e85c5c5476ecf2df4075dd0eb9
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-06-21 13:57:31 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 752 ms, Memory: 56188 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,36 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
-public class Main {
+class Main {
+    static class Node implements Comparable<Node>{
+        int x;
+        int y;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();		
-		int n = Integer.parseInt(br.readLine());
-		TreeMap<Integer, TreeSet<Integer>> tree = new TreeMap<>();
-		for (int i = 0 ; i < n ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			if(!tree.containsKey(y)) {
-				TreeSet<Integer> temp = new TreeSet<>();
-				temp.add(x);
-				tree.put(y, temp);
-			} else {
-				tree.get(y).add(x);
-			}
-		}
-		
-		for(Integer y : tree.keySet()) {
-			for(Integer x : tree.get(y)) {
-				sb.append(x + " " + y + "\n");
-			}
-		}
-		System.out.println(sb.toString());
-	}
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.y!=o.y?this.y-o.y:this.x-o.x;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        while(n-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.add(new Node(x,y));
+        }
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            sb.append(node.x+" "+ node.y+"\n");
+        }
+        System.out.println(sb);
+
+    }
 
 }
