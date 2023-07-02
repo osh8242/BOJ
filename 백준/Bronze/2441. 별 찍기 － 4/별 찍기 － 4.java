44a8a71d0bb4e5df2f7d100b822bb52988672347
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-07-02 11:55:31 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                sb.append(" ");
+            }
+            for(int j = i ; j < n ; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+}
