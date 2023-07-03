41aeacd494f8a3933edeb51229c54782285ecf45
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-07-03 09:25:29 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 136 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1 ; i <= 2*n-1 ; i++){
+            int index = (i>n?2*n-i:i) -1;
+            for(int j = 1 ; j <= index ; j++){
+                sb.append(" ");
+            }
+            for(int j = 1 ; j <= 2*n-1-2*index ; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
