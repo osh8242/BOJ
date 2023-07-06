e2931b0dc3e76c8b864f14aadece406015e6c52c
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-07-06 11:30:39 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int requireMoney = k*n;
+        if(requireMoney > m) System.out.println(requireMoney-m);
+        else System.out.println(0);
+    }
+}
