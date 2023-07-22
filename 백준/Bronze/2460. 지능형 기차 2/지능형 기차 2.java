c6e42a392ed6d0d49475d932f5c64db7326f1239
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-07-22 14:11:25 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.MIN_VALUE;
+        int num = 0;
+        for(int i = 1 ; i <= 10 ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            num += -Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            max = num > max ? num : max;
+        }
+        System.out.println(max);
+    }
+}
