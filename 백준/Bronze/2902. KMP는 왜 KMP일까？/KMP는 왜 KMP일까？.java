8513263d1da22ea846047c8fcfe7af2a591d9328
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-08-08 09:00:55 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),"-");
+        StringBuilder sb = new StringBuilder();
+        while(st.hasMoreTokens()){
+            sb.append(st.nextToken().charAt(0));
+        }
+        System.out.println(sb);
+    }
+}
