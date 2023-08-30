f3674140ad4dfeac0adcd67b96f3288b22e27d4c
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-08-30 09:13:16 +0900

    [Bronze II] Title: 만취한 상범, Time: 120 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append((int)Math.sqrt(n)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
