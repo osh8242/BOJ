cdecf0a08c5e59953039f0a2e46e54ec7d299e10
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-09-16 17:37:41 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -5,21 +5,21 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine());
+        long S = Long.parseLong(br.readLine());
         long sum = 0;
         int c = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= S; i++) {
             sum += i;
             c += 1;
-            if (n == 2) {
+            if (S == 2) {
                 System.out.println(1);
                 break;
             }
-            if (sum == n) {
+            if (sum == S) {
                 System.out.println(c);
                 break;
-            } else if (sum > n) {
+            } else if (sum > S) {
                 System.out.println(c - 1);
                 break;
             }
