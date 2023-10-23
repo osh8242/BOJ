93387d94daedf82557a7ca359bf7caf2c5349414
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-10-23 20:36:25 +0900

    [Gold IV] Title: 좋다, Time: 192 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] nums;
+    static int N;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(nums);
+        for (int i = 0; i < N; i++) {
+            if (isGoodNumber(nums[i], i)) count++;
+        }
+        System.out.println(count);
+    }
+
+    static boolean isGoodNumber(int number, int currentIndex) {
+        int startIndex = 0;
+        int endIndex = N - 1;
+        int sum = nums[startIndex] + nums[endIndex];
+        while (startIndex < endIndex) {
+            if (nums[startIndex] + nums[endIndex] == number) {
+                if (startIndex != currentIndex && endIndex != currentIndex) {
+                    return true;
+                } else if (startIndex == currentIndex) startIndex++;
+                else endIndex--;
+            } else if (nums[startIndex] + nums[endIndex] < number) {
+                startIndex++;
+            } else {
+                endIndex--;
+            }
+        }
+        return false;
+    }
+
+}
