16bffd77e394431b4fbfa279e42f8af1b704a915
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-10-26 23:57:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 664 ms, Memory: 48328 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,36 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static PriorityQueue<Point> pq = new PriorityQueue<>();
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();		
-		int n = Integer.parseInt(br.readLine());
-		TreeMap<Integer, TreeSet<Integer>> tree = new TreeMap<>();
-		for (int i = 0 ; i < n ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			if(!tree.containsKey(x)) {
-				TreeSet<Integer> temp = new TreeSet<>();
-				temp.add(y);
-				tree.put(x, temp);
-			} else {
-				tree.get(x).add(y);
-			}
-		}
-		
-		for(Integer x : tree.keySet()) {
-			for(Integer y : tree.get(x)) {
-				sb.append(x + " " + y + "\n");
-			}
-		}
-		System.out.println(sb.toString());
-	}
+    static class Point implements Comparable<Point> {
+        private final int x;
+        private final int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.x < o.x ? -1 : this.x == o.x ? this.y < o.y ? -1 : 1 : 1;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.add(new Point(x, y));
+        }
+        while (!pq.isEmpty()) {
+            Point point = pq.poll();
+            sb.append(point.x).append(" ").append(point.y).append("\n");
+        }
+        System.out.println(sb);
+
+    }
 
 }
