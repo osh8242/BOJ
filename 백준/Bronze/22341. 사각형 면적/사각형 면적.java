6a460739868d936927cdd7e7e5dee0935b6c010f
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-10-28 15:23:47 +0900

    [Bronze II] Title: 사각형 면적, Time: 196 ms, Memory: 19200 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, C;
+    static int currentX, currentY;
+
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        currentX = N;
+        currentY = N;
+        C = Integer.parseInt(st.nextToken());
+        while (C-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x >= currentX || y >= currentY) continue;
+            if (y * currentX >= x * currentY) currentY = y;
+            else currentX = x;
+        }
+        System.out.println(currentX * currentY);
+
+    }
+
+}
