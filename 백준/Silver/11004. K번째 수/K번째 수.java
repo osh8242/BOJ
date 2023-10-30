d66c70a88888653d5d0e99318c3c979192ea8ef4
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-10-31 07:11:42 +0900

    [Silver V] Title: K번째 수, Time: 2140 ms, Memory: 377624 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, K;
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(nums);
+        System.out.println(nums[K - 1]);
+    }
+
+}
