0044a5deae2bb990ae1dca24f565817bae4e2bfc
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-01 16:48:30 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 920 ms, Memory: 100964 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,23 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.TreeSet;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, K;
+    static int[] array, temp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		TreeSet<Integer> tree = new TreeSet<>();
-		int n = Integer.parseInt(br.readLine());
-		for (int i = 0 ; i < n ; i++) {
-			int temp = Integer.parseInt(br.readLine());
-			tree.add(temp);
-		}
-		for (Integer num : tree) {
-			sb.append(num+"\n");
-		}
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        array = new int[N];
+        temp = new int[N];
+        for (int i = 0; i < N; i++) array[i] = Integer.parseInt(br.readLine());
+        mergeSort(0, N - 1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) sb.append(array[i]).append("\n");
+        System.out.println(sb);
+    }
+
+    static void mergeSort(int start, int end) {
+        if (start < end) {
+            int middle = (start + end) / 2;
+            mergeSort(start, middle);
+            mergeSort(middle + 1, end);
+
+            int left = start;
+            int right = middle + 1;
+            int index = start;
+            while (left <= middle || right <= end) {
+                if (right > end || (left <= middle && array[left] < array[right]))
+                    temp[index++] = array[left++];
+                else temp[index++] = array[right++];
+            }
+            for (int i = start; i <= end; i++) array[i] = temp[i];
+        }
+    }
 
 }
