09c1c83a4319420716a25a9bd3880cfbd3c6ae1f
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-03 04:17:56 +0900

    [Silver IV] Title: 수 찾기, Time: 592 ms, Memory: 44612 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,64 +1,66 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int[] nums, temp;
+    static int[] testNums;
+    static StringBuilder sb = new StringBuilder();
 
-	public static char[] documents;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        temp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(br.readLine());
+        testNums = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) testNums[i] = Integer.parseInt(st.nextToken());
+        mergeSort(0, N - 1);
+        for (int i = 0; i < M; i++) {
+            if (binarySearch(testNums[i]) != -1) sb.append(1);
+            else sb.append(0);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static void mergeSort(int start, int end) {
+        if (start < end) {
+            int middle = start + (end - start) / 2;
+            mergeSort(start, middle);
+            mergeSort(middle + 1, end);
 
-		int n = Integer.parseInt(br.readLine());
+            int left = start;
+            int right = middle + 1;
+            int index = start;
+            while (left <= middle || right <= end) {
+                if (left > middle || (right <= end && nums[left] > nums[right]))
+                    temp[index++] = nums[right++];
+                else temp[index++] = nums[left++];
+            }
+            System.arraycopy(temp, start, nums, start, end + 1 - start);
+        }
+    }
 
-		long[] a = new long[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			a[i] = Long.parseLong(st.nextToken());
-		}
-
-		int m = Integer.parseInt(br.readLine());
-
-		long[] nums = new long[m];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < m; i++) {
-			nums[i] = Long.parseLong(st.nextToken());
-		}
-
-		Arrays.sort(a);
-		int[] count = new int[m];
-		for (int i = 0; i < m; i++) {
-			if (isExist(a, nums[i])) {
-				count[i] = 1;
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < m; i++) {
-			sb.append(count[i] + "\n");
-		}
-		System.out.println(sb.toString());
-
-	}
-
-	public static boolean isExist(long[] a, long num) {
-		int min = -1;
-		int max = a.length;
-		int p = (max + min) / 2;
-		while (max - min > 1) {
-			if (a[p] > num) {
-				max = p;
-				p = (max + min) / 2;
-				continue;
-			} else if (a[p] < num) {
-				min = p;
-				p = (max + min) / 2;
-				continue;
-			} else {
-				return true;
-			}
-		}
-		return false;
-	}
+    static int binarySearch(int num) {
+        int left = 0;
+        int right = N - 1;
+        int middle = left + (right - left) / 2;
+        while (left < right) {
+            if (nums[middle] > num) {
+                right = middle;
+                middle = left + (right - left) / 2;
+            } else if (nums[middle] < num) {
+                left = middle + 1;
+                middle = left + (right - left) / 2;
+            } else return middle;
+        }
+        return nums[middle] == num ? middle : -1;
+    }
 
 }
