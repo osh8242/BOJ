eba568572b8df2df30edbdb82eb387de3959b62a
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-04 02:09:09 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,41 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, m;
-    public static int[][] map;
-    public static int[] dr = {1, 0, -1, 0};
-    public static int[] dc = {0, 1, 0, -1,};
-    public static int answer;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static boolean[][] isVisitable;
+    static boolean[][] isVisited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static class Horse {
+        private int x;
+        private int y;
+        private int distance;
+
+        public Horse(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n+2][m+2];
-        answer = n*m;
-        for(int i =1 ; i <= n ; i++){
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isVisitable = new boolean[N + 2][M + 2];
+        isVisited = new boolean[N + 2][M + 2];
+        for (int i = 1; i <= N; i++) {
             String line = br.readLine();
-            for (int j = 1 ; j <= m ; j++){
-                if(line.charAt(j-1) == '1') map[i][j] = 1;
+            for (int j = 0; j < M; j++) {
+                if (line.charAt(j) == '1') isVisitable[i][j + 1] = true;
             }
         }
-        dfs(1,1,1);
-        System.out.println(answer);
-
+        System.out.println(bfs());
     }
 
-    public static void dfs(int i, int j, int depth){
-        if(i==n && j==m && depth < answer) answer = depth;
-        if(map[i][j] == 1 || map[i][j] > depth){
-            map[i][j] = depth;
-            depth++;
-            for(int d = 0 ; d < 4 ; d++){
-                if(map[i+dr[d]][j+dc[d]] == 1 || map[i+dr[d]][j+dc[d]] > depth) dfs(i+dr[d], j+dc[d], depth);
+    static int bfs() {
+        Queue<Horse> que = new LinkedList<>();
+        isVisited[1][1] = true;
+        que.add(new Horse(1, 1, 1));        
+        while (!que.isEmpty()) {
+            Horse horse = que.poll();
+            int x = horse.x;
+            int y = horse.y;
+            int distance = horse.distance;
+            if (x == N && y == M)
+                return distance;            
+            for (int i = 0; i < 4; i++) {
+                if (!isVisited[x + dx[i]][y + dy[i]] && isVisitable[x + dx[i]][y + dy[i]]) {
+                    isVisited[x+dx[i]][y+dy[i]] = true;
+                    que.add(new Horse(x + dx[i], y + dy[i], distance + 1));
+                }
             }
         }
+        return -1;
     }
+
 }
