e78be3737119dc060820e48c27e8d0a08bed0814
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-05 07:37:43 +0900

    [Silver I] Title: 기타 레슨, Time: 332 ms, Memory: 24308 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -7,7 +7,6 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N, M;
     static int[] lectures;
-    static int totalTime = 0;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,48 +14,29 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         lectures = new int[N];
         st = new StringTokenizer(br.readLine());
-        lectures[0] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i < N; i++) {
-            lectures[i] = Integer.parseInt(st.nextToken());
-            totalTime += lectures[i];
-        }
-        int minSize = getMinSize(M, totalTime);
-        System.out.println(minSize);
-
-
-    }
-
-    static int getMinSize(int blurayNum, int totalTime) {
-        int testSize = totalTime / M;
-        if (!canRecord(blurayNum, testSize)) {
-            testSize++;
-            while (true) {
-                if (!canRecord(blurayNum, testSize)) testSize++;
-                else break;
-            }
-        } else {
-            testSize--;
-            while (true) {
-                if (canRecord(blurayNum, testSize)) testSize--;
-                else break;
-            }
-        }
-        return testSize;
-    }
-
-    static boolean canRecord(int blurayNum, int size) {
-        int blurayCount = 1;
-        int tempSum = 0;
+        int start = 0;
+        int end = 0;
         for (int i = 0; i < N; i++) {
-            if (tempSum + lectures[i] <= size) tempSum += lectures[i];
-            else {
-                if (size < lectures[i]) return false;
-                blurayCount++;
-                tempSum = 0;
-                i--;
-            }
+            lectures[i] = Integer.parseInt(st.nextToken());
+            start = Math.max(start, lectures[i]);
+            end += lectures[i];
         }
-        return blurayCount <= blurayNum;
-    }
 
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (sum + lectures[i] > middle) {
+                    count++;
+                    sum = 0;
+                }
+                sum += lectures[i];
+            }
+            if (sum != 0) count++;
+            if (count > M) start = middle + 1;
+            else end = middle - 1;
+        }
+        System.out.println(start);
+    }
 }
