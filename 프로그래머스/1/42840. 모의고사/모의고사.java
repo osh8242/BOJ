1c3525cc6331f106d43b8d3fa5fb06b9b7c05ab9
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-08 10:28:12 +0900

    [level 1] Title: 모의고사, Time: 3.76 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,15 +7,15 @@ class Solution {
         submit[2] = new int[]{2,1,2,3,2,4,2,5};
         submit[3] = new int[]{3,3,1,1,2,2,4,4,5,5};
         
-        int[] answer = new int[4];     
+        int[] count = new int[4];     
         for(int i = 0 ; i < answers.length ; i++){
             for(int j = 1 ; j <= 3 ; j++){
-                if(answers[i] == submit[j][i%submit[j].length]) answer[j]++;
+                if(answers[i] == submit[j][i%submit[j].length]) count[j]++;
             }
         }
-        int max = Math.max(answer[1],Math.max(answer[2],answer[3]));        
-        return IntStream.range(0, answer.length)
-                .filter(i -> answer[i] == max)
+        int max = Math.max(count[1],Math.max(count[2],count[3])); 
+        return IntStream.range(0, count.length)
+                .filter(i -> count[i] == max)
                 .toArray();
     }
 }
