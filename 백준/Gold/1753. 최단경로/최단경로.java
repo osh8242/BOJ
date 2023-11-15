9a07170c05b79b1bf17490cbf7fb438774ad2344
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-15 11:08:43 +0900

    [Gold IV] Title: 최단경로, Time: 724 ms, Memory: 108632 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -6,73 +6,67 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Node implements Comparable<Node>{
-        public int index;
-        public int weight;
-        public Node(int index, int weight){
-            this.index = index;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.weight-o.weight;
-        }
-    }
-    static int v, e, k; //정점의 수, 간선의 수, 시작정점
-    static ArrayList<Node>[] paths;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int V, E, K;
+    static ArrayList<Path>[] paths;
+    static boolean[] isVisited;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        v = Integer.parseInt(st.nextToken());
-        paths = new ArrayList[v+1];
-        for(int i = 1 ; i <= v ; i++) paths[i] = new ArrayList<>();
-        e = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(br.readLine());
-        for(int line = 1 ; line <= e ; line++){
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        paths = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) paths[i] = new ArrayList<>();
+        K = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= E; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            paths[u].add(new Node(v,w));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            paths[start].add(new Path(end, cost));
         }
 
-        int[] minWeights = dijkstra();
-        for(int i = 1 ; i <= v ; i++){
-            if(minWeights[i]==Integer.MAX_VALUE) sb.append("INF\n");
-            else sb.append(minWeights[i] + "\n");
-        }
-        System.out.println(sb);
-
-    }//main(){}
-
-    static int[] dijkstra(){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] minWeights = new int[v+1];
-        for(int i = 1 ; i <= v ; i++) minWeights[i] = Integer.MAX_VALUE;
-        pq.add(new Node(k,0));
-        minWeights[k] = 0;
-        while(!pq.isEmpty()){
-            Node node = pq.poll();
-            int currentIndex = node.index;
-            int currentWeight = node.weight;
-            if(minWeights[currentIndex] < currentWeight) continue;
-
-            ArrayList<Node> cNodes = paths[currentIndex];
-            for(Node cNode : cNodes){
-                int newIndex = cNode.index;
-                int newWeight = minWeights[currentIndex]+cNode.weight;
-                if(minWeights[newIndex] > newWeight){
-                    minWeights[newIndex] = newWeight;
-                    pq.add(new Node(newIndex, newWeight));
+        PriorityQueue<Path> que = new PriorityQueue<>();
+        int[] costs = new int[V + 1];
+        isVisited = new boolean[V + 1];
+        for (int i = 1; i <= V; i++) costs[i] = Integer.MAX_VALUE;
+        que.offer(new Path(K, 0));
+        costs[K] = 0;
+        while (!que.isEmpty()) {
+            Path current = que.poll();
+            if (isVisited[current.index]) continue;
+            isVisited[current.index] = true;
+            for (Path next : paths[current.index]) {
+                int newCost = costs[current.index] + next.cost;
+                if (costs[next.index] > newCost) {
+                    costs[next.index] = newCost;
+                    que.offer(new Path(next.index, newCost));
                 }
             }
         }
 
-        return minWeights;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            sb.append(costs[i] == Integer.MAX_VALUE ? "INF" : costs[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 
-}//class Main{}
+    static class Path implements Comparable<Path> {
+        private int index;
+        private int cost;
+
+        public Path(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Path o) {
+            return this.cost - o.cost;
+        }
+    }
+
+}
