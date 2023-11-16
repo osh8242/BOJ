071de39211abc75fa2c0357475386bb15c40404d
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-16 18:32:29 +0900

    [Gold IV] Title: 플로이드, Time: 452 ms, Memory: 43832 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -3,72 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
-    static class Node{
-        private int index;
-        private int cost;
-        public Node(int index, int cost) {
-            this.index = index;
-            this.cost = cost;
-        }
-    }
-
-    static int n,m;
-    static int[][] paths;
-    static StringBuilder sb;
+public class Main {
+    static int N, M;
+    static int[][] graph;
+    static final int INF = 10000001;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        paths = new int[n+1][n+1];
-        while(m-->0){
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= N; j++)
+                if (i == j) graph[i][j] = 0;
+                else graph[i][j] = INF;
+
+        while (M-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            if(paths[start][end] == 0 || paths[start][end] > cost){
-                paths[start][end] = cost;
-            }
+            graph[start][end] = Math.min(graph[start][end], cost);
         }
-        sb = new StringBuilder();
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                if(i==j) continue;
-                if(isConnected(i,j)){
-                    fw(i,j);
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
             }
         }
-        print();
-
-    }
-
-    static boolean isConnected(int start, int end){
-        return paths[start][end]>0 ? true : false;
-    }
-
-    static void print(){
-        for (int i = 1 ; i <=n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                sb.append(paths[i][j]+" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sb.append(graph[i][j] == INF ? "0" : graph[i][j]).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
+
     }
 
-    static void fw(int i, int j){
-        for(int k = 1 ; k <= n ; k++){
-            if(i==k) continue;
-            if(isConnected(j,k)){
-                int newCost = paths[i][j]+paths[j][k];
-                if(paths[i][k] == 0 || paths[i][k] > newCost){
-                    paths[i][k] = newCost;
-                    fw(i,k);
-                }
-            }
-        }
-    }
 }
