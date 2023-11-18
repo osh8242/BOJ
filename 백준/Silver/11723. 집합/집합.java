9d36e4b290c0e9dd99d441a1e9f3851cb7a65d2f
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-11-18 12:32:22 +0900

    [Silver V] Title: 집합, Time: 1204 ms, Memory: 314532 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,37 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
         StringBuilder sb = new StringBuilder();
-        boolean[] set = new boolean[21];
-        int n = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < n ; i++){
-            String[] command = br.readLine().split(" ");
-            if(command[0].equals("add")){
-                set[Integer.parseInt(command[1])] = true;
-            } else if(command[0].equals("remove")){
-                set[Integer.parseInt(command[1])] = false;
-            } else if(command[0].equals("check")){
-                if(set[Integer.parseInt(command[1])]) sb.append("1\n");
-                else sb.append("0\n");
-            } else if(command[0].equals("toggle")){
-                set[Integer.parseInt(command[1])] = !set[Integer.parseInt(command[1])];
-            } else if(command[0].equals("all")){
-                for(int j = 1 ; j <= 20 ; j++){
-                    set[j] = true;
-                }
-            } else if(command[0].equals("empty")){
-                for(int j = 1 ; j <= 20 ; j++){
-                    set[j] = false;
-                }
+        while(M-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int num = -1;
+            if(st.hasMoreTokens()) num = Integer.parseInt(st.nextToken());
+            switch (command){
+                case "add" : set.add(num); break;
+                case "remove" : set.remove(num); break;
+                case "check" : sb.append(set.contains(num) ? "1" : "0").append("\n"); break;
+                case "toggle" : if(set.contains(num)) set.remove(num); else set.add(num); break;
+                case "all" : for(int i = 1 ; i <= 20 ; i++) set.add(i); break;
+                case "empty" : set.clear();
             }
-
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
 
     }
 
