4e08fad965a708e28d2d162061befd995149db82
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-12-09 13:35:18 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,16 +1,8 @@
 import math
 
 
-def main():
-    r, b = map(int, input().split())
-    # 2 * w + 2 * h - 4 = r;
-    # (w-2)*(h-2) = b;
-    # (r-2*h)*(h-2) = 2*b;
-    # - 2*h^2 + (4 + r) * h  -2*r = 2*b
-    determinant = (4 + r) ** 2 - 4 * 2 * (2 * r + 2 * b)
-    max = (-(4 + r) - math.sqrt(determinant)) / -4
-    print(str(int(max)) + " " + str(int((r - 2 * max + 4) / 2)))
+r, b = map(int, input().split())
+determinant = (4 + r) ** 2 - 4 * 2 * (2 * r + 2 * b)
+max = (-(4 + r) - math.sqrt(determinant)) / -4
+print(str(int(max)) + " " + str(int((r - 2 * max + 4) / 2)))
 
-
-if __name__ == "__main__":
-    main()
