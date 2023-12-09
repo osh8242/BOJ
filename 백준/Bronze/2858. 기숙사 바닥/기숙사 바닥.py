881da8a609677f1df4fe078a609a9d32f1a93ec7
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-12-09 18:57:34 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,8 +1,5 @@
 import math
-
-
 r, b = map(int, input().split())
 determinant = (4 + r) ** 2 - 4 * 2 * (2 * r + 2 * b)
 max = (-(4 + r) - math.sqrt(determinant)) / -4
 print(str(int(max)) + " " + str(int((r - 2 * max + 4) / 2)))
-
