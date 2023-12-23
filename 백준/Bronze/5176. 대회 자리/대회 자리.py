31bf93258e483eecf6f834c1f9354c7328860bcb
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-12-23 10:40:23 +0900

    [Bronze II] Title: 대회 자리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5176. 대회 자리/대회 자리.py
+++ 백준/Bronze/5176. 대회 자리/대회 자리.py
@@ -1,11 +1,17 @@
-T = int(input())
+import sys
+
+
+def readline(): return sys.stdin.readline()
+
+
+T = int(readline())
 result = []
 for _ in range(T):
-    P, M = map(int, input().split())
+    P, M = map(int, readline().split())
     seat = [False for _ in range(M+1)]
     answer = 0
     for i in range(0, P):
-        seat_wanted = int(input())
+        seat_wanted = int(readline())
         if seat[seat_wanted]:
             answer += 1
         else:
