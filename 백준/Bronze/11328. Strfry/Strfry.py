e4151c2a708f3c588c8a82091b551933badd63f8
osh8242 <127957174+osh8242@users.noreply.github.com>
2023-12-27 19:49:39 +0900

    [Bronze II] Title: Strfry, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,6 +1,13 @@
-T = int(input())
+import sys
+
+
+def readline():
+    return sys.stdin.readline().strip()
+
+
+T = int(readline())
 result = ""
 for _ in range(T):
-    strArr = list(map(sorted, input().split()))
+    strArr = list(map(sorted, readline().split()))
     result += ("Possible" if strArr[0] == strArr[1] else "Impossible") + "\n"
 print(result)
