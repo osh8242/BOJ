2e1fed6df909cc8eb04882f29ba690ea909aa481
osh8242 <osh8242@gmail.com>
2024-01-03 21:10:20 +0900

    feat: add python file

--- Main.java
+++ Main.java
@@ -1,47 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] dnaRef;
-    static int count = 0;
+class Main {
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        String dnaString = br.readLine();
-        st = new StringTokenizer(br.readLine());
-        dnaRef = new int[4];
-        for (int i = 0; i < 4; i++) dnaRef[i] = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < dnaString.length() - P + 1; i++) {
-            if (validatePassword(dnaString.substring(i, i + P))) count++;
-        }
-        System.out.println(count);
-    }
-
-    static boolean validatePassword(String pString) {
-        int[] dnaCount = dnaRef.clone();
-        for (int i = 0; i < pString.length(); i++) {
-            switch (pString.charAt(i)) {
-                case 'A':
-                    dnaCount[0]--;
-                    break;
-                case 'C':
-                    dnaCount[1]--;
-                    break;
-                case 'G':
-                    dnaCount[2]--;
-                    break;
-                case 'T':
-                    dnaCount[3]--;
-                    break;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        while (true) {
+            line = br.readLine();
+            if (line.charAt(0) == '#') break;
+            char[] charArr = line.toCharArray();
+            int index = 1;
+            int answer = 0;
+            for (char ch : charArr) {
+                if (ch != ' ') answer += index * (ch - 64);
+                index++;
             }
-            if (dnaCount[0] <= 0 && dnaCount[1] <= 0 && dnaCount[2] <= 0 && dnaCount[3] <= 0) return true;
+            sb.append(answer).append("\n");
         }
-        return false;
+        System.out.println(sb);
     }
 
 }
