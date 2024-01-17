367b97dad2a7ee0499277d30de9aa7006e3468df
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-01-18 00:06:04 +0900

    [Bronze I] Title: 2진수 8진수, Time: 336 ms, Memory: 23512 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         char[] binaryArr = br.readLine().toCharArray();
-        int length = binaryArr.length;
-        for (int i = length >= 3 ? length -3 : 0 ; i >= 0; ) {
-            int tempSum = 0;
-            char[] temp = Arrays.copyOfRange(binaryArr, i, i + (i != 0 || length % 3 == 0 ? 3 : length % 3));
-
-            int index = 1;
-            for (int j = temp.length - 1; j >= 0; j--) {
-                tempSum += index * (temp[j] - 48);
-                index *= 2;
-            }
-            sb.insert(0, tempSum);
-            if (i == 0) break;
-            i -= Math.min(i, 3);
+        int residue = binaryArr.length % 3;
+        if (residue % 3 == 1) {
+            sb.append(binaryArr[0]);
+        }
+        if (residue % 3 == 2) {
+            sb.append(2 * (binaryArr[0] - '0') + (binaryArr[1] - '0'));
+        }
+        for (int i = residue; i < binaryArr.length; i += 3) {
+            sb.append(binaryArr[i + 2] - '0' + 2 * (binaryArr[i + 1] - '0') + 4 * (binaryArr[i] - '0'));
         }
         System.out.println(sb);
     }
