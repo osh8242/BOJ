b0a5485562708316e03bf73a8a6adf512ad204be
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-01-27 10:24:08 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((long) N * (N - 1) * (N + 1) / 2);
+    }
+}
