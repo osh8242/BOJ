59e1484d7f1447ef30fdd5d95b8e89a4913588bf
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-02-03 18:36:11 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 132 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.java
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -10,31 +8,43 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         int totalUnit = 0;
         double totalPoint = 0;
-        while(N-->0){
+        while (N-- > 0) {
             String[] line = br.readLine().split(" ");
             int unit = Integer.parseInt(line[1]);
             totalUnit += unit;
             char[] gradeArr = line[2].toCharArray();
             char grade = gradeArr[0];
-            char param = gradeArr.length > 1 ? gradeArr[1] : 0;            
+            char param = gradeArr.length > 1 ? gradeArr[1] : 0;
             totalPoint += unit * getPoint(grade, param);
         }
-        System.out.printf("%.2f",totalPoint / totalUnit);
+        System.out.printf("%.2f", totalPoint / totalUnit);
     }
 
-    static double getPoint (char ch, char param){
+    static double getPoint(char ch, char param) {
         double result = 0;
 
-        switch (ch){
-            case 'A' : result = 4.0; break;
-            case 'B' : result = 3.0; break;
-            case 'C' : result = 2.0; break;
-            case 'D' : result = 1.0; break;
+        switch (ch) {
+            case 'A':
+                result = 4;
+                break;
+            case 'B':
+                result = 3;
+                break;
+            case 'C':
+                result = 2;
+                break;
+            case 'D':
+                result = 1;
+                break;
         }
 
-        switch (param){
-            case '+' : result += 0.3; break;
-            case '-' : result -= 0.3; break;
+        switch (param) {
+            case '+':
+                result += 0.3;
+                break;
+            case '-':
+                result -= 0.3;
+                break;
         }
 
         return result;
