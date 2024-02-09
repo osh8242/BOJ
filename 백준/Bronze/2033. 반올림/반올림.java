e34aa9e1092cc9dc428094707caae8ea375e90d7
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-02-09 10:53:06 +0900

    [Bronze I] Title: 반올림, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2033. 반올림/반올림.java
+++ 백준/Bronze/2033. 반올림/반올림.java
@@ -11,7 +11,7 @@ class Main {
         char[] input = br.readLine().toCharArray();
 
         // 각 자릿수 ArrayList
-        List<Integer> digits = new ArrayList<>(input.length);
+        List<Integer> digits = new ArrayList<>(2 * input.length);
         for (int i = input.length - 1; i >= 0; i--) {
             digits.add(input[i] - '0');
         }
