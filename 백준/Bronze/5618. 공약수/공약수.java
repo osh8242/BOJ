cba84f1bed5c1f181764455eedb190c4d3b0933c
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-02-12 21:04:26 +0900

    [Bronze II] Title: 공약수, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -1,27 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 class Main {
     static StringBuilder sb = new StringBuilder();
     static int[] nums;
-    static TreeSet<Integer> answer = new TreeSet<>();
+    static List<Integer> fisrtHalf = new ArrayList<>();
+    static Stack<Integer> lastHalf = new Stack<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        nums = new int[N];
+        int[] nums = new int[N];
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         int gcd = getGCD(nums[0], nums[1]);
         if (N == 3) gcd = getGCD(gcd, nums[2]);
         findCommonDivisor(gcd);
-        for(Integer num : answer){
+        for (Integer num : fisrtHalf) {
             sb.append(num).append("\n");
+        }
+        while(!lastHalf.isEmpty()){
+            sb.append(lastHalf.pop()).append("\n");
         }
         System.out.println(sb);
     }
@@ -32,11 +38,14 @@ class Main {
     }
 
     static void findCommonDivisor(int num) {
-        for (int i = 1; i <= Math.sqrt(num); i++)
+        int sqrt = (int) Math.sqrt(num);
+        for (int i = 1; i <= sqrt; i++) {
             if (num % i == 0) {
-                answer.add(i);
-                answer.add(num/i);
+                fisrtHalf.add(i);
+                lastHalf.push(num / i);
             }
+        }
+        if (sqrt * sqrt == num) lastHalf.pop();
     }
 
 }
