9dfe2fd7c7e5cedffdc7abb5c9963f9345fa2c4d
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-02-17 10:37:50 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            char[] chars = br.readLine().toCharArray();
+            chars[0] = (char) (chars[0] >= 'a' ? chars[0]- 32 : chars[0]);
+            sb.append(chars).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
