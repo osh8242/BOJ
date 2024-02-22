0227b206c2ec3a0db3c61dc4660a4dd1864e31c1
osh8242 <osh8242@gmail.com>
2024-02-22 23:48:27 +0900

    11057번. 오르막 수

--- Main.java
+++ Main.java
@@ -1,27 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+
 
 class Main {
+    static int N;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-        String[] input = br.readLine().split(" ");
-        boolean existZero = false;
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(input[i]);
-            existZero = num == 0 || existZero;
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        int max = 0;
-        for (int num : map.keySet()) {
-            if (num == map.get(num) && num > max) max = num;
-        }
-        System.out.println(max != 0 ? max : existZero ? -1 : 0);
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][10];
+        for (int i = 0; i < 10; i++) dp[0][i] = 1;
+        findAscendingNumbers();
+        System.out.println(dp[N][0]);
     }
 
+    static void findAscendingNumbers() {
+        for (int i = 1; i < N + 1; i++) {
+            dp[i][9] = 1;
+            for (int j = 8; j >= 0; j--) {
+                dp[i][j] = dp[i-1][j] + dp[i][j+1];
+                dp[i][j] %= 10007;
+            }
+        }
+    }
 }
