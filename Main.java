61c2759e9b39fbd9e8110e0ac1586b93a2b27111
osh8242 <osh8242@gmail.com>
2024-03-03 19:56:35 +0900

    12015번. 가장 긴 증가하는 부분 수열2

--- Main.java
+++ Main.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N;
-    static int[] dp, nums;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        nums = new int[N];
-        dp = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> dp = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-            dp[i] = nums[i];
-        }
-
-        int max = dp[0];
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[j] + nums[i], dp[i]);
-                    max = Math.max(dp[i], max);
+        dp.add(Integer.parseInt(st.nextToken()));
+        while (st.hasMoreTokens()) {
+            int value = Integer.parseInt(st.nextToken());
+            if(value > dp.get(dp.size()-1)) dp.add(value);
+            else {
+                int left = 0;
+                int right = dp.size()-1;
+                while(left < right){
+                    int mid = (left+right)/2;
+                    int midValue = dp.get(mid);
+                    if(value > midValue) left = mid+1;
+                    else right = mid;
                 }
+                dp.set(left, value);
             }
         }
-        System.out.println(max);
+        System.out.println(dp.size());
     }
-
 }
