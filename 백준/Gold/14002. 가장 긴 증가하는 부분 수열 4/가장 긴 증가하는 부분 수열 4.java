5fe07b9c745a2182239799c14466706bc70cf41b
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-03-05 06:38:04 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 140 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -8,45 +8,57 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        int[] input = new int[N];
+        int[] inputs = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N ; i++) input[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < N ; i++) inputs[i] = Integer.parseInt(st.nextToken());
 
-        int[] lis = new int[N];
-        lis[0] = 1;
-        List<Integer> dp = new ArrayList<>();
-        dp.add(input[0]);
+        // dp 시작 세팅
+        List<Integer> dp = new ArrayList<>(N); // LIS를 담는 List
+        int[] rank = new int[N];// i번째 숫자가 가장 클 떄의 LIS 길이를 담는 배열
 
+        // 초기값 세팅
+        dp.add(inputs[0]); // 첫번째 숫자를 dp에 담아준다
+        rank[0] = 1; // 첫번째 숫자는 LIS 길이가 1
+        int maxRank = 1;
 
-        for(int i = 1 ; i < N ; i++) {
-            if(input[i] > dp.get(dp.size()-1)) {
-                dp.add(input[i]);
-                lis[i] = dp.size();
-            }
-            else {
+        // dp 시작
+        for(int i = 1;  i < N; i++){
+            int value = inputs[i];
+
+            // i번째 숫자가 dp의 마지막 값보다 크면 dp에 담고 LIS 길이는 dp 사이즈와 같다
+            if(value > dp.get(dp.size()-1)) {
+                dp.add(value);
+                rank[i] = dp.size();
+
+            // i번째 숫자가 dp 마지막 값보다 작으면
+            // dp에 담긴 숫자 중에 i번째 숫자보다 같거나 큰 수들 중 가장 작은 작은 숫자와 치환
+            // i번째 숫자가 가장 클때의 LIS 길이는 dp에서 찾은 위치와 같음
+            } else {
                 int left = 0;
                 int right = dp.size()-1;
-                while(left < right){
+                while(left<right){
                     int mid = (left+right)/2;
                     int midValue = dp.get(mid);
-                    if(input[i] > midValue) left = mid+1;
+                    if(value > midValue) left = mid+1;
                     else right = mid;
                 }
-                dp.set(left, input[i]);
-                lis[i] = left + 1;
+                dp.set(left, value);
+                rank[i] = left+1;
             }
+            // 가장 긴 LIS 값과 비교하여 업데이트
+            maxRank = Math.max(rank[i], maxRank);
         }
-        sb.append(dp.size()).append("\n");
-
-        int btValue = dp.size();
+        // 백트래킹 시작
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
-        for(int i = N -1 ; i >= 0 ; i--){
-            if(lis[i] == btValue) {
-                stack.push(input[i]);
-                btValue--;
+        sb.append(maxRank).append("\n");
+        for(int i = N-1; i >=0 ; i--){
+            if(rank[i] == maxRank) {
+                stack.push(inputs[i]);
+                maxRank--;
             }
         }
         while(!stack.isEmpty()) sb.append(stack.pop()).append(" ");
