aa2d45bec750c77004afeed3cd1200d5efd3ff0f
osh8242 <osh8242@gmail.com>
2024-03-09 01:32:00 +0900

    11944번. NN

--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> dp = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        dp.add(Integer.parseInt(st.nextToken()));
-        while (st.hasMoreTokens()) {
-            int value = Integer.parseInt(st.nextToken());
-            if(value > dp.get(dp.size()-1)) dp.add(value);
-            else {
-                int left = 0;
-                int right = dp.size()-1;
-                while(left < right){
-                    int mid = (left+right)/2;
-                    int midValue = dp.get(mid);
-                    if(value > midValue) left = mid+1;
-                    else right = mid;
-                }
-                dp.set(left, value);
-            }
+        String N = st.nextToken();
+        int M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < Integer.parseInt(N) ; i++){
+            sb.append(N);
+            if(sb.length() > M) break;
         }
-        System.out.println(dp.size());
+        System.out.println(sb.length() > M ? sb.substring(0, M) : sb);
     }
 }
