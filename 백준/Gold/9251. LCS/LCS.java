095efeef64a1df261b5ec013fe198dfa1d5b173c
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-03-14 06:32:22 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18284 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -11,13 +11,15 @@ public class Main {
         int[][] dp = new int[chars1.length + 1][chars2.length + 1];
         for (int i = 1; i <= chars1.length; i++) {
             for (int j = 1; j <= chars2.length; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                if (chars1[i-1] == chars2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
         System.out.println(dp[chars1.length][chars2.length]);
     }
-    
+
+
 }
