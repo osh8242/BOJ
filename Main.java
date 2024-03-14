46e9787b9ae40e5beb4b911b9f10ee3e1d7a6d90
osh8242 <osh8242@gmail.com>
2024-03-15 00:02:35 +0900

    10830번. 행렬 제곱 https://www.acmicpc.net/problem/10830
    11444번. 피보나치 수 6
    https://www.acmicpc.net/problem/11444

--- Main.java
+++ Main.java
@@ -4,16 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static long[][] A = {{1, 1}, {1, 0}};
+    static long N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String N = st.nextToken();
-        int M = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < Integer.parseInt(N) ; i++){
-            sb.append(N);
-            if(sb.length() > M) break;
-        }
-        System.out.println(sb.length() > M ? sb.substring(0, M) : sb);
+        N = Integer.parseInt(st.nextToken());
+        long[][] answer = getPowSquare(N);
+        System.out.println(answer[1][0]);
     }
+
+    static long[][] getPowSquare(long n) {
+        if (n == 1) return A;
+        long[][] childMatrix = getPowSquare(n / 2);
+        long[][] result = multipleMatrix(childMatrix, childMatrix);
+        if (n % 2 == 1) result = multipleMatrix(result, A);
+        return result;
+    }
+
+    static long[][] multipleMatrix(long[][] X, long[][] Y) {
+        long[][] result = new long[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += X[i][k] * Y[k][j];
+                    result[i][j] %= 1000000007;
+                }
+            }
+        }
+        return result;
+    }
+
 }
