5d001c2ec353f67944e376be4a28ed869b52c44d
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-03-24 10:30:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 568 ms, Memory: 48288 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -5,17 +5,19 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-static int V, E;
-static int[] parent;
+    static int V, E;
+    static int[] parent, rank;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        parent = new int[V+1];
-        for(int i = 1 ; i <= V ; i++){
+        parent = new int[V + 1];
+        rank = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
             parent[i] = i;
+            rank[i] = 0;
         }
         PriorityQueue<Edge> queue = new PriorityQueue<Edge>();
         while (E-- > 0) {
@@ -26,9 +28,9 @@ static int[] parent;
             queue.offer(new Edge(v1, v2, w));
         }
         int answer = 0;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Edge edge = queue.poll();
-            if(find(edge.v1) != find(edge.v2)){
+            if (find(edge.v1) != find(edge.v2)) {
                 union(edge.v1, edge.v2);
                 answer += edge.w;
             }
@@ -36,18 +38,27 @@ static int[] parent;
         System.out.println(answer);
     }
 
-    static int find(int v){
-        if(v == parent[v]) return v;
+    static int find(int v) {
+        if (v == parent[v]) return v;
         return parent[v] = find(parent[v]);
     }
 
-    static void union(int v1, int v2){
-        int parent1 = find(v1);
-        int parent2 = find(v2);
-        if(parent1 != parent2) parent[parent2] = parent1;
+    static void union(int v1, int v2) {
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (root1 != root2) {
+            if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+            } else if (rank[root1] < rank[root2]) {
+                parent[root1] = root2;
+            } else {
+                parent[root2] = root1;
+                rank[root1]++;
+            }
+        }
     }
 
-    static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge> {
         int v1, v2, w;
 
         public Edge(int v1, int v2, int w) {
@@ -55,6 +66,7 @@ static int[] parent;
             this.v2 = v2;
             this.w = w;
         }
+
         @Override
         public int compareTo(Edge o) {
             return Integer.compare(this.w, o.w);
