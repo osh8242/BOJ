2369c05a71bc1762018a6e88c712b68b60dcd0af
osh8242 <osh8242@gmail.com>
2024-04-04 21:12:44 +0900

    10569번. 다면체 https://www.acmicpc.net/problem/10569

--- Main.java
+++ Main.java
@@ -1,44 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] chars = br.readLine().toCharArray();
-        int N = chars.length;
-        boolean[][] isPalindrome = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            isPalindrome[i][i] = true;
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(2 - Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
         }
-        for (int i = 0; i < N - 1; i++) {
-            if (chars[i] == chars[i + 1]) {
-                isPalindrome[i][i + 1] = true;
-            }
-        }
-        for (int j = 2; j < N; j++) {
-            for (int i = 0; i < N - 2; i++) {
-                if (chars[i] == chars[j] && isPalindrome[i + 1][j - 1]) {
-                    isPalindrome[i][j] = true;
-                }
-            }
-        }
-        int[] dp = new int[N];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        for (int idx = 0; idx < N; idx++) {
-            if (isPalindrome[0][idx]) {
-                dp[idx] = 1;
-                continue;
-            }
-            for (int s = 1; s <= idx; s++) {
-                if (isPalindrome[s][idx]) {
-                    dp[idx] = Math.min(dp[idx], dp[s - 1] + 1);
-                }
-            }
-        }
-        System.out.println(dp[N - 1]);
+        System.out.println(sb);
     }
 }
-
