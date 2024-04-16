644d12f9fb0d9a96a7ee02c7034aa8bb721becd6
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-04-17 06:20:08 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+for _ in range(n):
+    c, v = map(int, input().split())
+    print(f'You get {c // v} piece(s) and your dad gets {c % v} piece(s).')
