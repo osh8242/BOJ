2ef8776d7c910da2236b0b1b7c5c93dc180e9b2f
osh8242 <osh8242@gmail.com>
2024-05-19 04:26:17 +0900

    2765번. 자전거 속도 https://www.acmicpc.net/problem/2765

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
-N = int(input())
-q = N // 2 + 1
-print(q * q if N % 2 == 0 else q * (q+1))
+import math
+import sys
+
+
+def read_line():
+    return sys.stdin.readline()
+
+
+N = 1
+while True:
+    r, s, t = map(float, read_line().split())
+    if s == 1:
+        break
+    mile = math.pi * r * s / 63360
+    rpm = mile / t * 60 * 60
+    print(f"Trip #{N}: {mile:.2f} {rpm:.2f}")
