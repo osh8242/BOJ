9820eef2f750a50d44f8e2fbd87efb28afe1a855
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-05-18 09:29:57 +0900

    [Bronze III] Title: Crowing, Time: 0 ms, Memory: 1408 KB -BaekjoonHub

---
+++ 백준/Bronze/2321. Crowing/Crowing.f
@@ -0,0 +1,12 @@
+PROGRAM AsciiArt
+  IMPLICIT NONE
+
+  ! 각 라인 출력
+  PRINT '(a)', ',~'
+  PRINT '(a)', '( 9> //)'
+  PRINT '(a)', ' )(_///'
+  PRINT '(a)', '( \_>/'
+  PRINT '(a)', ' \__/'
+  PRINT '(a)', '  ^^'
+
+END PROGRAM AsciiArt
