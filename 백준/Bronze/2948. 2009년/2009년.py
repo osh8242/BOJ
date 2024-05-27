55fd130c1fde663f84782793b26658737c14d34b
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-05-28 06:36:23 +0900

    [Bronze II] Title: 2009년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2948. 2009년/2009년.py
+++ 백준/Bronze/2948. 2009년/2009년.py
@@ -1,4 +1,4 @@
-days_of_month = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
-day = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
+m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+d = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
 D, M = map(int, input().split())
-print(day[(days_of_month[M-1] + D) % 7])
+print(d[(m[M-1] + D) % 7])
