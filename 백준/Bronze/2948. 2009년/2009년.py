4f1f5fd5fce391595cd8acf9d8f810ff68536dfa
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-05-28 06:35:23 +0900

    [Bronze II] Title: 2009년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2948. 2009년/2009년.py
+++ 백준/Bronze/2948. 2009년/2009년.py
@@ -1,5 +1,4 @@
-import sys
 days_of_month = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
 day = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
-D, M = map(int, sys.stdin.readline().split())
+D, M = map(int, input().split())
 print(day[(days_of_month[M-1] + D) % 7])
