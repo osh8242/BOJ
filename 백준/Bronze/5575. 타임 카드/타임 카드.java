21eb51c74f6c2ea14012fb59c58b2b88b0739ac9
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-02 12:13:12 +0900

    [Bronze IV] Title: 타임 카드, Time: 120 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/5575. 타임 카드/타임 카드.java
+++ 백준/Bronze/5575. 타임 카드/타임 카드.java
@@ -4,49 +4,33 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static class Time {
-        private int hour;
-        private int min;
-        private int sec;
-
-        public Time(int hour, int min, int sec) {
-            this.hour = hour;
-            this.min = min;
-            this.sec = sec;
-        }
-
-        public Time getDuration(Time end) {
-            int sec = 0;
-            int min = 0;
-            int hour = 0;
-            sec += end.sec - this.sec;
-            if (sec < 0) {
-                sec += 60;
-                min--;
-            }
-            min += end.min - this.min;
-            if (min < 0) {
-                min += 60;
-                hour--;
-            }
-            hour += end.hour - this.hour;
-            return new Time(hour, min, sec);
-        }
-
-        public String toString(){
-            return String.format("%d %d %d", hour, min, sec);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 3; i++) {
             String[] input = br.readLine().split(" ");
-            Time start = new Time(Integer.parseInt(input[0]), Integer.parseInt(input[1]), Integer.parseInt(input[2]));
-            Time end = new Time(Integer.parseInt(input[3]), Integer.parseInt(input[4]), Integer.parseInt(input[5]));
-            Time duration = start.getDuration(end);
-            System.out.println(duration);
+            int durationInSeconds = getDurationInSeconds(input);
+
+            int hours = durationInSeconds / 3600;
+            durationInSeconds %= 3600;
+            int minutes = durationInSeconds / 60;
+            int seconds = durationInSeconds % 60;
+
+            sb.append(hours).append(" ").append(minutes).append(" ").append(seconds).append("\n");
         }
+        System.out.println(sb);
     }
 
+    private static int getDurationInSeconds(String[] input) {
+        int startHour = Integer.parseInt(input[0]);
+        int startMin = Integer.parseInt(input[1]);
+        int startSec = Integer.parseInt(input[2]);
+        int endHour = Integer.parseInt(input[3]);
+        int endMin = Integer.parseInt(input[4]);
+        int endSec = Integer.parseInt(input[5]);
+
+        int startInSeconds = startHour * 3600 + startMin * 60 + startSec;
+        int endInSeconds = endHour * 3600 + endMin * 60 + endSec;
+        return endInSeconds - startInSeconds;
+    }
 }
