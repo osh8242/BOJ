0b17e03ab43cfd16cf4a293b591e021540939622
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-10 06:35:36 +0900

    [Bronze II] Title: 3의 배수, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/16561. 3의 배수/3의 배수.java
+++ 백준/Bronze/16561. 3의 배수/3의 배수.java
@@ -1,14 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            int n = Integer.parseInt(br.readLine()) / 3;
-            bw.write(String.valueOf((n - 1) * (n - 2) / 2));
-            bw.flush();
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()) / 3;
+        System.out.println((n - 1) * (n - 2) / 2);
     }
 
 }
