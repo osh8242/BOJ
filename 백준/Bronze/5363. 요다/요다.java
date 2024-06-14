79efeb5c44169f11152bc247938763b620b823c3
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-14 09:36:36 +0900

    [Bronze II] Title: 요다, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/5363. 요다/요다.java
+++ 백준/Bronze/5363. 요다/요다.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
+            StringBuilder sb = new StringBuilder();
             int N = Integer.parseInt(br.readLine());
             while (N-- > 0) {
                 String input = br.readLine();
@@ -14,9 +15,9 @@ public class Main {
                 input = input.substring(input.indexOf(" ") + 1);
                 String word2 = input.substring(0, input.indexOf(" ") + 1);
                 input = input.substring(input.indexOf(" ") + 1);
-                StringBuilder sb = new StringBuilder(input).append(" ").append(word1).append(word2).append("\n");
-                bw.write(sb.toString());
+                sb.append(input).append(" ").append(word1).append(word2).append("\n");                
             }
+            bw.write(sb.toString());
             bw.flush();
         }
     }
