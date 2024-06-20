abf16614887ac8bc287340fae26916ba7ca6acba
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-20 20:53:16 +0900

    [Bronze IV] Title: 선물, Time: 284 ms, Memory: 23492 KB -BaekjoonHub

--- 백준/Bronze/28281. 선물/선물.java
+++ 백준/Bronze/28281. 선물/선물.java
@@ -1,21 +1,30 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        int[] prices = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        System.out.println(
-                IntStream.range(0, N - 1)
-                .map(i -> prices[i] + prices[i + 1])
-                .min().getAsInt() * X);
+        
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int minCost = Integer.MAX_VALUE;
+        for (int i = 0; i < N - 1; i++) {
+            int cost = X * (A[i] + A[i + 1]);
+            if (cost < minCost) {
+                minCost = cost;
+            }
+        }
+        
+        System.out.println(minCost);
     }
-
 }
