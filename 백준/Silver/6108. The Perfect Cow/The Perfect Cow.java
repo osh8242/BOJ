10610ebc4247d35d06c4ff0bd336dfb95fe575e7
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-29 20:51:47 +0900

    [Silver V] Title: The Perfect Cow, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Silver/6108. The Perfect Cow/The Perfect Cow.java
+++ 백준/Silver/6108. The Perfect Cow/The Perfect Cow.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
@@ -7,11 +9,15 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] median = new int[N];
         for (int i = 0; i < N; i++) {
-            int[] row = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] row = new int[N];
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < line.length; j++) {
+                row[j] = Integer.parseInt(line[j]);
+            }
             Arrays.sort(row);
-            median[i] = row[row.length/2];
+            median[i] = row[row.length / 2];
         }
         Arrays.sort(median);
-        System.out.println(median[median.length/2]);
+        System.out.println(median[median.length / 2]);
     }
 }
