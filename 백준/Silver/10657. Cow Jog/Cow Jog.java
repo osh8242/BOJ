c413709eafa18c32d274aeedbcc9fb08be3f9300
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-06-30 13:17:07 +0900

    [Silver IV] Title: Cow Jog, Time: 404 ms, Memory: 52408 KB -BaekjoonHub

--- 백준/Silver/10657. Cow Jog/Cow Jog.java
+++ 백준/Silver/10657. Cow Jog/Cow Jog.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Cow> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Cow[] cows = new Cow[N];
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
-            pq.offer(new Cow(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
+            cows[i] = new Cow(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
         }
         int groupCount = 1;
-        Cow tail = pq.poll();
-        while (!pq.isEmpty()) {
-            Cow current = pq.poll();
+        Cow tail = cows[N - 1];
+        for (int i = N - 2; i >= 0; i--) {
+            Cow current = cows[i];
             if (current.speed > tail.speed) {
                 current.speed = tail.speed;
                 tail = current;
