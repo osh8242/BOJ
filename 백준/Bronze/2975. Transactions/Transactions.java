f971cd75ab99548357fbfb4a32b7916a6cac8e20
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-07-03 06:43:04 +0900

    [Bronze III] Title: Transactions, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/2975. Transactions/Transactions.java
+++ 백준/Bronze/2975. Transactions/Transactions.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String line;
         while (!(line = br.readLine()).equals("0 W 0")) {
             StringTokenizer st = new StringTokenizer(line);
@@ -21,7 +20,7 @@ public class Main {
             }
             sb.append("\n");
         }
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
     }
-
 }
