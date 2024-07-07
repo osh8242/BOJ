5210fa9d985b25e8debc11cd5863257fafd2c2a1
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-07-07 10:46:21 +0900

    [Silver V] Title: 콰트로치즈피자, Time: 112 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.java
+++ 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.java
@@ -10,17 +10,15 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        boolean isPossible = false;
         while (st.hasMoreTokens()) {
             String topping = st.nextToken();
             if(topping.endsWith("Cheese")) set.add(topping);
             if(set.size() == 4) {
-                isPossible = true;
                 break;
             }
         }
 
-        bw.write(isPossible ? "yummy" : "sad");
+        bw.write(set.size() == 4 ? "yummy" : "sad");
         bw.flush();
     }
 }
