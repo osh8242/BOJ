de5f850360e7ec982fcc3dfa13c14678ae9395ae
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-07-09 06:20:23 +0900

    [Bronze III] Title: Triangles, Time: 124 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/5356. Triangles/Triangles.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            char ch = st.nextToken().charAt(0);
+            appendTriangle(N, ch, sb);
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void appendTriangle(int N, char ch, StringBuilder sb) {
+        for (int i = 1; i <= N ; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append(ch);
+            }
+            sb.append("\n");
+            ch += (char) (ch == 90 ? -25 : 1);
+        }
+    }
+
+
+}
