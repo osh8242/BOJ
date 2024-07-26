635d5197352c4f1fef4ac59a73376b035f5d7be7
osh8242 <osh8242@gmail.com>
2024-07-27 00:34:53 +0900

    11648번. 지속 https://www.acmicpc.net/problem/11648

--- Main.java
+++ Main.java
@@ -7,13 +7,30 @@ public class Main {
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             StringBuilder sb = new StringBuilder();
 
-            int N = Integer.parseInt(br.readLine());
-            int count = 0;
-            for (int i = 0; i < 3; i++) if (br.readLine().contains("7")) count++;
+            String num = br.readLine();
+            int count = getStageCount(num);
 
-            bw.write(count == 3 ? "777" : "0");
+            bw.write(Integer.toString(count));
             bw.flush();
         }
+    }
+
+    static int getStageCount(String num) {
+        int count = 0;
+        int value = Integer.parseInt(num);
+        while (value >= 10) {
+            value = proceedStage(Integer.toString(value));
+            count++;
+        }
+        return count;
+    }
+
+    static int proceedStage(String num) {
+        int answer = Character.getNumericValue(num.charAt(0));
+        for(int i = 1; i < num.length(); i++) {
+            answer *= Character.getNumericValue(num.charAt(i));
+        }
+        return answer;
     }
 
 }
