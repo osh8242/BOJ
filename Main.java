7e66e811892f28b0342c7585d6c635fb405c0f31
osh8242 <osh8242@gmail.com>
2024-07-30 00:49:22 +0900

    10353번. Frame https://www.acmicpc.net/problem/10353

--- Main.java
+++ Main.java
@@ -1,36 +1,32 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int X, Y;
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-            String num = br.readLine();
-            int count = getStageCount(num);
-
-            bw.write(Integer.toString(count));
-            bw.flush();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            sb.append(
+                            isPossible(Integer.parseInt(br.readLine())) ?
+                                    "YES" : "NO")
+                    .append("\n");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
     }
 
-    static int getStageCount(String num) {
-        int count = 0;
-        int value = Integer.parseInt(num);
-        while (value >= 10) {
-            value = proceedStage(Integer.toString(value));
-            count++;
-        }
-        return count;
+    static boolean isPossible(int k) {
+        if (X % k == 0 && (Y - 2) % k == 0) return true;
+        if ((X - 1) % k == 0 && (Y - 1) % k == 0) return true;
+        return (X - 2) % k == 0 && Y % k == 0;
     }
-
-    static int proceedStage(String num) {
-        int answer = Character.getNumericValue(num.charAt(0));
-        for(int i = 1; i < num.length(); i++) {
-            answer *= Character.getNumericValue(num.charAt(i));
-        }
-        return answer;
-    }
-
 }
