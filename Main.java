21e8f03c12cfd26f75b8336a24bbc2bcf1d393d6
osh8242 <osh8242@gmail.com>
2024-07-30 01:09:06 +0900

    17599번. Bags https://www.acmicpc.net/problem/17599

--- Main.java
+++ Main.java
@@ -1,32 +1,24 @@
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int X, Y;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            sb.append(
-                            isPossible(Integer.parseInt(br.readLine())) ?
-                                    "YES" : "NO")
-                    .append("\n");
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
-        bw.write(sb.toString());
+        bw.write(String.valueOf(set.size()));
         bw.flush();
     }
 
-    static boolean isPossible(int k) {
-        if (X % k == 0 && (Y - 2) % k == 0) return true;
-        if ((X - 1) % k == 0 && (Y - 1) % k == 0) return true;
-        return (X - 2) % k == 0 && Y % k == 0;
-    }
 }
