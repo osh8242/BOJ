0cc06f2fa1d58c6e9eeefa6366a004a3356e8014
osh8242 <osh8242@gmail.com>
2024-08-17 22:06:41 +0900

    29700번. 우당탕탕 영화예매 https://www.acmicpc.net/board/view/129303

--- Main.java
+++ Main.java
@@ -1,25 +1,45 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[][] zeros;
+
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        int[] menu = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            menu[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        zeros = new int[N][M+1];
+        for (int i = 0; i < N; i++) {
+            readRowZeroCount(br.readLine(), i);
         }
-        int M = Integer.parseInt(br.readLine());
-        int sum = 0;
-        for (int i = 1; i <= M; i++) {
-            sum += menu[Integer.parseInt(br.readLine())];
+        int answer = 0;
+        for (int[] rowZeros : zeros) {
+            for(int j = K ; j <= M ; j++) {
+                answer += rowZeros[j] * (j - K + 1);
+            }
         }
-
-        bw.write(String.valueOf(sum));
+        bw.write(Integer.toString(answer));
         bw.flush();
+    }
+
+    static void readRowZeroCount(String line, int rowIndex) throws IOException {
+        boolean isZero = false;
+        int zeroCount = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '0') {
+                if (!isZero) isZero = true;
+                zeroCount++;
+            } else if (isZero) {
+                isZero = false;
+                zeros[rowIndex][zeroCount]++;
+                zeroCount = 0;
+            }
+        }
+        if(isZero) zeros[rowIndex][zeroCount]++;
     }
 
 }
