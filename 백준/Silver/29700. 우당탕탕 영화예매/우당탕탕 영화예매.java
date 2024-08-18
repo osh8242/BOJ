e4e02699d7648f2a9ab7d0047c8e00b10d44a049
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-08-18 12:49:49 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 316 ms, Memory: 28240 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -5,25 +5,21 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N, M, K;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        int answer = 0;
         for (int i = 0; i < N; i++) {
-            int[] rowZeros = readRowZeroCount(br.readLine());
-            for (int j = K; j <= M; j++) {
-                answer += rowZeros[j] * (j - K + 1);
-            }
+            readRowZeroCount(br.readLine());
         }
         bw.write(Integer.toString(answer));
         bw.flush();
     }
 
-    static int[] readRowZeroCount(String line) throws IOException {
-        int[] rowZeros = new int[M + 1];
+    static void readRowZeroCount(String line) throws IOException {
         boolean isZero = false;
         int zeroCount = 0;
         for (int i = 0; i < line.length(); i++) {
@@ -32,12 +28,15 @@ public class Main {
                 zeroCount++;
             } else if (isZero) {
                 isZero = false;
-                rowZeros[zeroCount]++;
+                addCount(zeroCount);
                 zeroCount = 0;
             }
         }
-        if (isZero) rowZeros[zeroCount]++;
-        return rowZeros;
+        if (isZero) addCount(zeroCount);
+    }
+
+    static void addCount(int zeroCount) {
+        if (zeroCount >= K) answer += zeroCount - K + 1;
     }
 
 }
