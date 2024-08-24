f54c438bdabae6f1a437ae8a034c440a02487b9b
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-08-25 03:21:20 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,12 +1,19 @@
 import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static long N, answer;
 
     public static void main(String[] args) throws IOException {
-        N = Long.parseLong(br.readLine());
+        N = 0;
+        int input;
+
+        while ((input = System.in.read()) != '\n') {
+            if (input >= '0' && input <= '9') {
+                N = N * 10 + (input - '0');
+            }
+        }
+
         answer = -1;
         long q = N / 5;
         int r = (int) N % 5;
@@ -17,8 +24,8 @@ public class Main {
             case 3 -> answer = q + 1;
             case 4 -> {if (q > 0) answer = q + 2;}
         }
+
         bw.write(Long.toString(answer));
         bw.flush();
     }
-
 }
