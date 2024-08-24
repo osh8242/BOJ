f9462e8562c6572f5d3c53cdd4dd5ddb137aaa3c
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-08-25 03:22:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,24 @@
 import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static long N, answer;
 
     public static void main(String[] args) throws IOException {
-        N = 0;
-        int input;
-
-        while ((input = System.in.read()) != '\n') {
-            if (input >= '0' && input <= '9') {
-                N = N * 10 + (input - '0');
-            }
-        }
-
+        N = Long.parseLong(br.readLine());
         answer = -1;
         long q = N / 5;
         int r = (int) N % 5;
         switch (r) {
-            case 0 -> answer = q;
-            case 1 -> {if (q > 0) answer = q + 1;}
-            case 2 -> {if (q > 1) answer = q + 2;}
-            case 3 -> answer = q + 1;
-            case 4 -> {if (q > 0) answer = q + 2;}
+            case 0 : answer = q; break;
+            case 1 : {if (q > 0) answer = q + 1; break;}
+            case 2 : {if (q > 1) answer = q + 2; break;}
+            case 3 : answer = q + 1; break;
+            case 4 : {if (q > 0) answer = q + 2; break;}
         }
-
         bw.write(Long.toString(answer));
         bw.flush();
     }
+
 }
