d5ab73b60620edc9601dd84abd884e47c3afe60c
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-08-28 06:22:48 +0900

    [Bronze II] Title: Fridge of Your Dreams, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/11104. Fridge of Your Dreams/Fridge of Your Dreams.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            sb.append(Integer.parseInt(br.readLine(), 2)).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
