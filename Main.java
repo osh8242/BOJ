e03f0ba40da678c0f70c7e3aaa008b51a629c136
osh8242 <osh8242@gmail.com>
2024-08-31 22:20:00 +0900

    25418번. 정수 a를 k로 만들기 https://www.acmicpc.net/problem/25418

--- Main.java
+++ Main.java
@@ -1,41 +1,57 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static final int MOD = 1000000000;
+    static boolean[] isVisit = new boolean[1000001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int p = Math.abs(n);
-        bw.write(n < 0 && n % 2 == 0 ? "-1" : n == 0 ? "0" : "1");
-        bw.newLine();
-        bw.write(Integer.toString(fibonacci(p)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int answer = -1;
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(A, 0));
+        isVisit[A] = true;
+        while (!que.isEmpty()) {
+            Node cur = que.poll();
+            if (cur.index == K) {
+                answer = cur.count;
+                break;
+            }
+            int nextIndex = cur.index + 1;
+            int count = cur.count + 1;
+            if (inBound(nextIndex) && !isVisit[nextIndex]) {
+                isVisit[nextIndex] = true;
+                que.offer(new Node(nextIndex, count));
+            }
+            nextIndex = cur.index * 2;
+            if (inBound(nextIndex) && !isVisit[nextIndex]) {
+                isVisit[nextIndex] = true;
+                que.offer(new Node(nextIndex, count));
+            }
+        }
+
+        bw.write(Integer.toString(answer));
         bw.flush();
     }
 
-    static int fibonacci(int p) {
-        if (p <= 1) return p;
-        long[][] F = {{1, 1}, {1, 0}};
-        pow(F, p - 1);
-        return (int) F[0][0];
+    static boolean inBound(int n) {
+        return n < 1000001;
     }
 
-    static void pow(long[][] A, int p) {
-        if (p <= 1) return;
-        pow(A, p / 2);
-        multiply(A, A);
-        if (p % 2 != 0) multiply(A, new long[][]{{1, 1}, {1, 0}});
-    }
+}
 
-    static void multiply(long[][] A, long[][] B) {
-        long x = A[0][0] * B[0][0] + A[0][1] * B[1][0];
-        long y = A[0][0] * B[0][1] + A[0][1] * B[1][1];
-        long z = A[1][0] * B[0][0] + A[1][1] * B[1][0];
-        long w = A[1][0] * B[0][1] + A[1][1] * B[1][1];
-        A[0][0] = x % MOD;
-        A[0][1] = y % MOD;
-        A[1][0] = z % MOD;
-        A[1][1] = w % MOD;
+class Node {
+    int index;
+    int count;
+
+    public Node(int index, int count) {
+        this.index = index;
+        this.count = count;
     }
 }
