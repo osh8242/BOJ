f652d58c3ff96a456bfed0bd18a9c67c2ce84802
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-09-02 20:26:03 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 176 ms, Memory: 55920 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -1,97 +1,59 @@
 import java.io.*;
-
 import java.util.LinkedList;
-
 import java.util.Queue;
-
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static boolean[] isVisit = new boolean[1000001];
+    static boolean[] isVisit;
+    static int A, K;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int A = Integer.parseInt(st.nextToken());
-
-        int K = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        isVisit = new boolean[K+1];
 
         int answer = -1;
-
         Queue<Node> que = new LinkedList<>();
-
         que.offer(new Node(A, 0));
-
         isVisit[A] = true;
-
         while (!que.isEmpty()) {
-
             Node cur = que.poll();
-
             if (cur.index == K) {
-
                 answer = cur.count;
-
                 break;
-
             }
-
             int nextIndex = cur.index + 1;
-
             int count = cur.count + 1;
-
             if (inBound(nextIndex) && !isVisit[nextIndex]) {
-
                 isVisit[nextIndex] = true;
-
                 que.offer(new Node(nextIndex, count));
-
             }
-
             nextIndex = cur.index * 2;
-
             if (inBound(nextIndex) && !isVisit[nextIndex]) {
-
                 isVisit[nextIndex] = true;
-
                 que.offer(new Node(nextIndex, count));
-
             }
-
         }
 
         bw.write(Integer.toString(answer));
-
         bw.flush();
-
     }
 
     static boolean inBound(int n) {
-
-        return n < 1000001;
-
+        return n <= K;
     }
 
 }
 
 class Node {
-
     int index;
-
     int count;
 
     public Node(int index, int count) {
-
         this.index = index;
-
         this.count = count;
-
     }
-
 }
