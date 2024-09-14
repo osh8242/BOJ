18765c474ee7904e02d33e1e75f7ba256a6ebdaf
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-09-14 09:00:12 +0900

    [Silver IV] Title: 30, Time: 152 ms, Memory: 16488 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        char[] nums = br.readLine().toCharArray();
+        int sum = 0;
+        for (char num : nums) sum += num;
+        sum -= '0' * nums.length;
+
+        if (sum % 3 != 0) bw.write("-1");
+        else {
+            Arrays.sort(nums);
+            if (nums[0] != '0') bw.write(Integer.toString(-1));
+            else bw.write(sb.append(new String(nums)).reverse().toString());
+        }
+        bw.flush();
+    }
+
+}
