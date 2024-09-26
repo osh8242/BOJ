58878d1163b4a28d78ae1a295d574d90431242ba
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-09-27 06:37:56 +0900

    [Bronze III] Title: Hot Dogs, Time: 68 ms, Memory: 12004 KB -BaekjoonHub

--- 백준/Bronze/5666. Hot Dogs/Hot Dogs.java
+++ 백준/Bronze/5666. Hot Dogs/Hot Dogs.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         String input;
@@ -17,9 +15,8 @@ public class Main {
             int P = Integer.parseInt(st.nextToken());
             sb.append(String.format("%.2f\n", H / P));
         }
-
-        bw.write(sb.toString());
-        bw.flush();
+           
+        System.out.print(sb);
     }
 
 }
