9cde5c4136aa64e11b88409850d25cc4e15e5f75
osh8242 <osh8242@gmail.com>
2024-09-28 00:25:18 +0900

    9387번. Gholam’s Simple Game https://www.acmicpc.net/problem/9387

--- Main.java
+++ Main.java
@@ -1,57 +1,64 @@
-import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] isVisit = new boolean[1000001];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static int m, n, index, total, answer;
+    static boolean[] isYellow;
+    static boolean direction;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            index = -1;
+            total = 0;
+            answer = 0;
 
-        int answer = -1;
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(A, 0));
-        isVisit[A] = true;
-        while (!que.isEmpty()) {
-            Node cur = que.poll();
-            if (cur.index == K) {
-                answer = cur.count;
-                break;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            isYellow = new boolean[m];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                int x = Integer.parseInt(st.nextToken());
+                if (x == 0) {
+                    isYellow[i] = true;
+                    total++;
+                } else if (x == 2) {
+                    direction = true;
+                    index = i;
+                } else if (x == 3) {
+                    direction = false;
+                    index = i;
+                }
             }
-            int nextIndex = cur.index + 1;
-            int count = cur.count + 1;
-            if (inBound(nextIndex) && !isVisit[nextIndex]) {
-                isVisit[nextIndex] = true;
-                que.offer(new Node(nextIndex, count));
-            }
-            nextIndex = cur.index * 2;
-            if (inBound(nextIndex) && !isVisit[nextIndex]) {
-                isVisit[nextIndex] = true;
-                que.offer(new Node(nextIndex, count));
-            }
+
+            goStep(index, direction);
+            int residue = n / m;
+            answer += residue * total;
+            n %= m;
+            if (residue % 2 == 0) direction = !direction;
+            index = direction ? 0 : m - 1;
+            goStep(index, direction);
+            sb.append(answer).append("\n");
         }
 
-        bw.write(Integer.toString(answer));
+        bw.write(sb.toString());
         bw.flush();
     }
 
-    static boolean inBound(int n) {
-        return n < 1000001;
+    static void goStep(int index, boolean direction) {
+        int diff = direction ? 1 : -1;
+        for (int i = index + diff; n > 0 && i >= 0 && i < m; i += diff) {
+            if (isYellow[i]) answer++;
+            n--;
+        }
     }
 
-}
-
-class Node {
-    int index;
-    int count;
-
-    public Node(int index, int count) {
-        this.index = index;
-        this.count = count;
-    }
 }
