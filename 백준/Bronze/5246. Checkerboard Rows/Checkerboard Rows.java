4aa4dad3b38647254d349783648c1da36acea4b1
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-01 15:03:36 +0900

    [Bronze III] Title: Checkerboard Rows, Time: 64 ms, Memory: 12748 KB -BaekjoonHub

--- 백준/Bronze/5246. Checkerboard Rows/Checkerboard Rows.java
+++ 백준/Bronze/5246. Checkerboard Rows/Checkerboard Rows.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,7 +19,9 @@ public class Main {
                 int row = Integer.parseInt(st.nextToken());
                 rowCount[row]++;
             }
-            sb.append(Arrays.stream(rowCount).max().getAsInt()).append("\n");
+            int max = 0;
+            for (int count : rowCount) max = Math.max(max, count);
+            sb.append(max).append("\n");
         }
 
         bw.write(sb.toString());
