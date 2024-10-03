d90ffd31fcc1803da722710f8c1637bd0497c08e
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-03 16:29:35 +0900

    [level 2] Title: H-Index, Time: 1.69 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,33 +1,37 @@
 import java.util.Arrays;
 
 public class Solution {
+    
+    static int sum = 0;
+    
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        
+
         int left = 0;
         int right = citations.length;
-        
+
         while (left <= right) {
             int h = (left + right) / 2;
             int count = countCitationsAtLeastH(citations, h);
-            
+
             if (count >= h) {
                 left = h + 1;
             } else {
                 right = h - 1;
             }
         }
-        
+
         return right;
     }
-    
-    private int countCitationsAtLeastH(int[] citations, int h) {
-        int count = 0;
-        for (int citation : citations) {
-            if (citation >= h) {
-                count++;
-            }
+
+    private int countCitationsAtLeastH(int[] citations, int h) {        
+        int left = 0;
+        int right = citations.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (citations[mid] >= h) right = mid;
+            else left = mid + 1;
         }
-        return count;
+        return citations.length - left;
     }
 }
