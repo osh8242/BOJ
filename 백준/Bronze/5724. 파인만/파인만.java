fb269bf7bff477588881c1f857af265a17276474
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-04 11:55:15 +0900

    [Bronze III] Title: 파인만, Time: 56 ms, Memory: 11340 KB -BaekjoonHub

--- 백준/Bronze/5724. 파인만/파인만.java
+++ 백준/Bronze/5724. 파인만/파인만.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
@@ -16,8 +13,7 @@ public class Main {
             sb.append(n * (n + 1) * (2 * n + 1) / 6).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
     }
 
 }
