fe7653a5544895c7889a430fff6077f85285710c
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-12 12:52:03 +0900

    [Bronze V] Title: Shares, Time: 96 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input;
+        while ((input = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(input);
+            int N = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            sb.append(S / (N + 1)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
