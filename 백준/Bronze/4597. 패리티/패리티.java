6159633f0c21556b9def2e2d279471809d2cf2c0
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-19 09:49:43 +0900

    [Bronze II] Title: 패리티, Time: 56 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/4597. 패리티/패리티.java
+++ 백준/Bronze/4597. 패리티/패리티.java
@@ -5,26 +5,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-
         String input;
+
         while (!(input = br.readLine()).equals("#")) {
             char[] chars = input.toCharArray();
             int count = 0;
-            for (int i = 0; i < chars.length - 1; i++) {
-                if (chars[i] == '1') count++;
-            }
-            if (chars[input.length() - 1] == 'e') {
-                if (count % 2 == 0) chars[input.length() - 1] = '0';
-                else chars[input.length() - 1] = '1';
-            } else {
-                if (count % 2 == 0) chars[input.length() - 1] = '1';
-                else chars[input.length() - 1] = '0';
-            }
+            
+            for (char c : chars) if (c == '1') count++;
+            
+            chars[chars.length - 1] = (chars[chars.length - 1] == 'e') 
+                                       ? (count % 2 == 0 ? '0' : '1') 
+                                       : (count % 2 == 0 ? '1' : '0');
             sb.append(chars).append("\n");
         }
+        
         bw.write(sb.toString());
         bw.flush();
     }
-
 }
-
