bccfffc9059eed112fbbaac649f177397a76bb33
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-10-31 08:31:44 +0900

    [Bronze I] Title: XORXORXOR, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if(C % 2 == 1) sb.append(A ^ B);
+        else sb.append(A);
+
+        bw.write(sb.toString().trim());
+        bw.flush();
+    }
+}
