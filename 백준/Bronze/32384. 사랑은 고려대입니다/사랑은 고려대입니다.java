c13e0c9690fc71e464e6fb469dcbd68097c49a28
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-11-01 19:34:29 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) sb.append("LoveisKoreaUniversity ");
+
+        bw.write(sb.toString().trim());
+        bw.flush();
+    }
+}
