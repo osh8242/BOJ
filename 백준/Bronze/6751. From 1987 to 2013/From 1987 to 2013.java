9ceb9296c30bc0a443b2f05a200c513c9544266f
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-11-15 06:36:06 +0900

    [Bronze II] Title: From 1987 to 2013, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/6751. From 1987 to 2013/From 1987 to 2013.java
+++ 백준/Bronze/6751. From 1987 to 2013/From 1987 to 2013.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
-        while (!isDistinct(++year)) {}
+        while (!isDistinct(++year)) {
+        }
         System.out.println(year);
     }
 
     static boolean isDistinct(int year) {
-        String temp = String.valueOf(year);
-        Set<Character> set = new HashSet<>();
-        for (char c : temp.toCharArray()) {
-            if (!set.add(c)) return false;
+        char[] temp = String.valueOf(year).toCharArray();
+        for (int i = 0; i < temp.length - 1; i++) {
+            for (int j = i + 1; j < temp.length; j++) {
+                if (temp[i] == temp[j]) return false;
+            }
         }
         return true;
     }
