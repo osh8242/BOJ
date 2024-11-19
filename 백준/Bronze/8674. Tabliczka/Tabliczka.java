79e58d7605b010330f670f1b6ebeb8d4d8e99ea1
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-11-20 06:22:24 +0900

    [Bronze IV] Title: Tabliczka, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if (a % 2 == 0 || b % 2 == 0) System.out.println(0);
+        else System.out.println(Math.min(a, b));
+    }
+}
