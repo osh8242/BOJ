29432393fe0e4f771bb17478d47e68d3f33c1561
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-11-19 20:13:03 +0900

    [Silver II] Title: 미로 탈출, Time: 184 ms, Memory: 20344 KB -BaekjoonHub

---
+++ 백준/Silver/31834. 미로 탈출/미로 탈출.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int answer;
+            if (onBoundary(S, N)) {
+                if (onBoundary(E, N)) answer = 0;
+                else answer = 1;
+            } else {
+                if (Math.abs(S - E) == 1) answer = 1;
+                else answer = 2;
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static boolean onBoundary(int idx, int N) {
+        return idx == 1 || idx == N;
+    }
+
+}
