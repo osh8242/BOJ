51d82911876167758df14eb3650fc6d28a176368
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-12-03 06:23:58 +0900

    [Bronze III] Title: 쿠키애호가, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/11134. 쿠키애호가/쿠키애호가.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            sb.append(N % C == 0 ? N / C : N / C + 1).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
