cf36c7b52a16df06713a93151688b9bc9e6f40da
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-12-05 20:18:12 +0900

    [Bronze III] Title: 포인터 공부, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/25703. 포인터 공부/포인터 공부.java
+++ 백준/Bronze/25703. 포인터 공부/포인터 공부.java
@@ -3,26 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        sb.append("int a;").append("\n");
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            appendNthString(sb, i);
-        }
-        System.out.println(sb);
-    }
-
-    static void appendNthString(StringBuilder sb, int n) {
-        sb.append("int ");
-        for (int i = 0; i < n; i++) sb.append("*");
-        sb.append("ptr");
-        if (n != 1) sb.append(n);
-        sb.append(" = &");
-        sb.append(n == 1 ? "a" : "ptr");
-        if (n > 2) sb.append(n - 1);
-        sb.append(";").append("\n");
-    }
-
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+       sb.append("int a;\n");
+       int N = Integer.parseInt(br.readLine());
+       String stars = new String(new char[N]).replace('\0', '*');
+       for (int n = 1; n <= N; n++) {
+           sb.append("int ");
+           sb.append(stars, 0, n);
+           sb.append("ptr");
+           if (n != 1) sb.append(n);
+           sb.append(" = &");
+           if (n == 1) {
+               sb.append("a");
+           } else {
+               sb.append("ptr");
+               if (n - 1 != 1) sb.append(n - 1);
+           }
+           sb.append(";\n");
+       }
+       System.out.print(sb);
+   }
 }
