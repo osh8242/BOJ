38c06fb97ad024443fef52aa23692bf26cf7a258
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-12-10 06:23:13 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 312 ms, Memory: 37680 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == X) X = b;
+            else if (b == X) X = a;
+        }
+        System.out.println(X);
+    }
+}
