125e2e947e4a61ef03d74b538fc9376bbc64ddd7
osh8242 <127957174+osh8242@users.noreply.github.com>
2024-12-24 06:24:47 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long start = System.nanoTime();
+        System.out.println(fibonacci(N) + " " + (N-2));
+        long end = System.nanoTime();
+    }
+
+    static int fibonacci(int n) {
+        if(n == 1 || n == 2) return 1;
+        int a = 1;
+        int b = 1;
+        int c = -1;
+        for(int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+}
