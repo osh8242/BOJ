78113ca09d7fa903ad2ad10a5ea6de59c42ef68d
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-01 18:07:40 +0900

    [Bronze II] Title: 9진수, Time: 64 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Bronze/14491. 9진수/9진수.java
+++ 백준/Bronze/14491. 9진수/9진수.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -14,5 +13,4 @@ public class Main {
         }
         System.out.println(answer);
     }
-
 }
