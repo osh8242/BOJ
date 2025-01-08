0d4002087b748102441b5f694a4e630d806b08f9
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-08 23:57:27 +0900

    [Bronze I] Title: 진법 변환 2, Time: 68 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,23 +1,36 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        int code = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            if (num % code < 10) sb.append(num % code);
-            else sb.append((char) (num % code + 55));
-            num -= num % code;
-            num /= code;
-        }
-        System.out.println(sb.reverse());
-    }
+        byte[] buffer = new byte[64];
+        int len = System.in.read(buffer);
+        
+        long N = 0;
+        int B = 0;
+        int idx = 0;
 
+        while (buffer[idx] != ' ') {
+            N = N * 10 + (buffer[idx] - '0');
+            idx++;
+        }
+        idx++;
+        while (idx < len && buffer[idx] != '\n') {
+            B = B * 10 + (buffer[idx] - '0');
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (N > 0) {
+            long remainder = N % B;
+            if (remainder >= 10) {
+                result.append((char) ('A' + (remainder - 10)));
+            } else {
+                result.append((char) ('0' + remainder));
+            }
+            N /= B;
+        }
+
+        System.out.write(result.reverse().toString().getBytes());
+    }
 }
