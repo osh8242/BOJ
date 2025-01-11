80921b6018316f4cbb45c408551142e13e80aaa7
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-11 10:31:08 +0900

    [Bronze II] Title: 전북대학교, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/14624. 전북대학교/전북대학교.java
+++ 백준/Bronze/14624. 전북대학교/전북대학교.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
@@ -22,8 +21,7 @@ public class Main {
                 sb.append("*\n");
             }
         }
-
-        bw.write(sb.toString());
-        bw.flush();
+        
+        System.out.println(sb);
     }
 }
