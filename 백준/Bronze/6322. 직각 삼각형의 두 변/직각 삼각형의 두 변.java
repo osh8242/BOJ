cca903df196ef5dcbf2ad71a635cab80100f9210
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-14 06:21:42 +0900

    [Bronze III] Title: 직각 삼각형의 두 변, Time: 68 ms, Memory: 11988 KB -BaekjoonHub

---
+++ 백준/Bronze/6322. 직각 삼각형의 두 변/직각 삼각형의 두 변.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input;
+        int index = 1;
+        while (true) {
+            input = br.readLine();
+            if (input.equals("0 0 0")) break;
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            boolean isNormal = false;
+            if (a == -1 && (b < c)) isNormal = true;
+            else if (b == -1 && (a < c)) isNormal = true;
+            else if (c == -1) isNormal = true;
+
+            sb.append("Triangle #").append(index++).append("\n");
+            if (isNormal) {
+                if (a == -1) {
+                    double result = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
+                    sb.append("a = ").append(String.format("%.3f", result)).append("\n");
+                } else if (b == -1) {
+                    double result = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+                    sb.append("b = ").append(String.format("%.3f", result)).append("\n");
+                } else {
+                    double result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                    sb.append("c = ").append(String.format("%.3f", result)).append("\n");
+                }
+            } else {
+                sb.append("Impossible.\n");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
