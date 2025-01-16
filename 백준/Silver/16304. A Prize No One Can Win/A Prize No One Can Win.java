77b201d5989221f30e7e8c064e4a810fbe8136a6
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-17 06:22:50 +0900

    [Silver V] Title: A Prize No One Can Win, Time: 316 ms, Memory: 35056 KB -BaekjoonHub

---
+++ 백준/Silver/16304. A Prize No One Can Win/A Prize No One Can Win.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[] price = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < price.length; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+        }
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            Arrays.sort(price);
+            int i = binarySearch(price, X);
+            System.out.println(i + 1);
+        }
+    }
+
+    static int binarySearch(int[] price, int X) {
+        int low = 0;
+        int high = price.length - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (price[mid] + price[mid+1] <= X) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
