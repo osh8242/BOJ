d8943363ee6f218c641d24eca62d40a534323b57
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-19 11:32:36 +0900

    [Bronze IV] Title: 팀 나누기, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Bronze/13866. 팀 나누기/팀 나누기.java
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] input = br.readLine().split(" ");
         int[] level = new int[4];
-        for (int i = 0; i < 4; i++) level[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(level);
-        System.out.println(Math.abs(level[3] + level[0] - level[1] - level[2]));
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            level[i] = Integer.parseInt(input[i]);
+            sum += level[i];
+            if (level[i] < min) min = level[i];
+            if (level[i] > max) max = level[i];
+        }
+        System.out.println(Math.abs(2 * (max + min) - sum));
     }
 }
