6bf0b5b9188ec418867a3fb4e280f675dcd085a8
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-19 23:34:51 +0900

    [Bronze I] Title: N번째 큰 수, Time: 108 ms, Memory: 13112 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -1,19 +1,23 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        while (T-->0){
+        while (T-- > 0) {
             Queue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while(st.hasMoreTokens()){
+            while (st.hasMoreTokens()) {
                 que.offer(Integer.parseInt(st.nextToken()));
             }
-            while(que.size() > 8) que.poll();
+            while (que.size() > 8) que.poll();
             sb.append(que.poll()).append("\n");
         }
         System.out.println(sb);
