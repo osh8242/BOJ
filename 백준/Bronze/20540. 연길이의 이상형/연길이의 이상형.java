04c7e9cc3a3683191f9b535b65e917c43f0b1673
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-21 06:40:26 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.java
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.java
@@ -1,26 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.function.Function;
 
 public class Main {
-    static Function<Character, Character>[] functions;
-
-    static {
-        functions = new Function[4];
-        functions[0] = ch -> ch == 'E' ? 'I' : 'E';
-        functions[1] = ch -> ch == 'S' ? 'N' : 'S';
-        functions[2] = ch -> ch == 'T' ? 'F' : 'T';
-        functions[3] = ch -> ch == 'J' ? 'P' : 'J';
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] mbti = br.readLine().toCharArray();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            sb.append(functions[i].apply(mbti[i]));
-        }
+        sb.append(mbti[0] == 'E' ? 'I' : 'E');
+        sb.append(mbti[1] == 'S' ? 'N' : 'S');
+        sb.append(mbti[2] == 'T' ? 'F' : 'T');
+        sb.append(mbti[3] == 'J' ? 'P' : 'J');
         System.out.println(sb);
     }
 }
