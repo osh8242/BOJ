f564a661a230d9f8bc86aa23aca19c8a0c4a67bf
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-26 22:51:51 +0900

    [Silver II] Title: 무한이진트리, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2078. 무한이진트리/무한이진트리.java
+++ 백준/Silver/2078. 무한이진트리/무한이진트리.java
@@ -12,24 +12,37 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         a = Long.parseLong(st.nextToken());
         b = Long.parseLong(st.nextToken());
+
         if (a == 1 && b == 1) {
             System.out.println("0 0");
             return;
         }
+
         long l = 0;
         long r = 0;
-        while (a > 1 || b > 1) {
+
+        while (!(a == 1 && b == 1)) {
             if (a > b) {
-                a -= b;
-                l++;
-                if (a == 1) { r += b - 1; break; }
+                if (b == 1) {
+                    l += (a - b);
+                    a = 1;
+                } else {
+                    long div = a / b;
+                    l += div;
+                    a -= (div * b);
+                }
             } else {
-                b -= a;
-                r++;
-                if (b == 1) { l += a - 1; break; }
+                if (a == 1) {
+                    r += (b - a);
+                    b = 1;
+                } else {
+                    long div = b / a;
+                    r += div;
+                    b -= (div * a);
+                }
             }
         }
+
         System.out.println(l + " " + r);
     }
-
 }
