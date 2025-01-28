56fda9129cac1d2b0abfbcf9970de99c9db01f28
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-01-29 06:03:23 +0900

    [Bronze II] Title: 친구, Time: 76 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Bronze/10864. 친구/친구.java
+++ 백준/Bronze/10864. 친구/친구.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,21 +11,18 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] graph = new List[N + 1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        int[] friends = new int[N + 1];
 
         for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+            friends[a]++;
+            friends[b]++;
         }
 
         for (int i = 1; i <= N; i++) {
-            sb.append(graph[i].size()).append("\n");
+            sb.append(friends[i]).append("\n");
         }
 
         System.out.println(sb);
