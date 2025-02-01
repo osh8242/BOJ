8415bc61d8c3504e5e1b2a4aa4422f381e63f10f
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-02-02 07:39:50 +0900

    [Silver IV] Title: Prosjek, Time: 64 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Silver/15577. Prosjek/Prosjek.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        double average = arr[0];
+        for (int i = 1; i < N; i++) {
+            average = (arr[i] + average) / 2;
+        }
+        System.out.println(average);
+    }
+
+}
