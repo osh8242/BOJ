697de36feffdce054f0b95b4c489d3ca7dc745c0
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-02-04 20:43:50 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 116 ms, Memory: 16872 KB -BaekjoonHub

--- 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         int half = N / 2;
         for (int i = 0; i < half; i++) {
             sb.append('a');
         }
-        sb.append(sb);
+        bw.write(sb.toString());
         if (N % 2 != 0) sb.append('a');
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
