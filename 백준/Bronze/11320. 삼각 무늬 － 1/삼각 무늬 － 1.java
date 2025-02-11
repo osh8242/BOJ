cead559f00290139a6dfb980d30205380031aec5
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-02-12 06:40:37 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
