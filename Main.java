9546e5a995126a8e763820eaf95bf715519fc03b
osh8242 <osh8242@gmail.com>
2025-03-01 13:49:18 +0900

    Revert "17094번. Serious Problem https://www.acmicpc.net/problem/17094"
    This reverts commit d9fab70bec72121e611ff06a194431047bf86ca5.

--- Main.java
+++ Main.java
@@ -1,20 +1,64 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static int m, n, index, total, answer;
+    static boolean[] isYellow;
+    static boolean direction;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        String s = br.readLine();
-        int num = 0;
-        int ch = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '2') num++;
-            else if (c == 'e') ch++;
+    public static void main(String[] args) throws Exception {
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            index = -1;
+            total = 0;
+            answer = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            isYellow = new boolean[m];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                int x = Integer.parseInt(st.nextToken());
+                if (x == 0) {
+                    isYellow[i] = true;
+                    total++;
+                } else if (x == 2) {
+                    direction = true;
+                    index = i;
+                } else if (x == 3) {
+                    direction = false;
+                    index = i;
+                }
+            }
+
+            goStep(index, direction);
+            int residue = n / m;
+            answer += residue * total;
+            n %= m;
+            if (residue % 2 == 0) direction = !direction;
+            index = direction ? 0 : m - 1;
+            goStep(index, direction);
+            sb.append(answer).append("\n");
         }
-        System.out.println(num > ch ? '2' : ch > num ? 'e' : "yee");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void goStep(int index, boolean direction) {
+        int diff = direction ? 1 : -1;
+        for (int i = index + diff; n > 0 && i >= 0 && i < m; i += diff) {
+            if (isYellow[i]) answer++;
+            n--;
+        }
     }
 
 }
