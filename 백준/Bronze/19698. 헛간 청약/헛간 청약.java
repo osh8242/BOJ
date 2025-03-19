6316524c90ec60c3a1523f04732dcbe946dcff7e
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-03-20 06:40:17 +0900

    [Bronze IV] Title: 헛간 청약, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int cowsBySpace = (W / L) * (H / L);
+        int result = Math.min(N, cowsBySpace);
+        System.out.println(result);
+    }
+}
