13ec33b290dad0fbec3484485ccb1bd7090c6e77
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-03-29 10:13:23 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 64 ms, Memory: 11492 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int x = N;
+        while (isPrime(x)) x++;
+        System.out.println(x);
+    }
+
+    public static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n % 2 == 0) return n == 2;
+        for (long i = 3; i * i <= n; i += 2)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+}
