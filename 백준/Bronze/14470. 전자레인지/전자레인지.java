3f237fede7135bf539869646b5b65ae29a88d143
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-04-04 06:23:57 +0900

    [Bronze IV] Title: 전자레인지, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+        
+        int time = 0;
+        if (A < 0) {
+            time += (-A) * C;
+            time += D;
+            time += B * E;
+        } else {
+            time += (B - A) * E;
+        }
+        
+        System.out.println(time);
+    }
+}
