8d6528bc1ec0a206ecf9603f6c1dc955b46f6dec
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-04-09 06:23:47 +0900

    [Bronze III] Title: 등차수열에서 항 번호 찾기, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/14913. 등차수열에서 항 번호 찾기/등차수열에서 항 번호 찾기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    int a = Integer.parseInt(st.nextToken());
+    int d = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    
+    if ((k - a) % d != 0) {
+      System.out.println("X");
+      return;
+    }
+    
+    int n = (k - a) / d + 1;
+    if (n < 1) {
+      System.out.println("X");
+    } else {
+      System.out.println(n);
+    }
+  }
+}
