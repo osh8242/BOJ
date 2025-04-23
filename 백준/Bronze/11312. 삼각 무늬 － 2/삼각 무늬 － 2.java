902e4233367b5ca73a3abba6711d7520d9978cb5
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-04-24 06:41:48 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 80 ms, Memory: 12108 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine().trim());
+        for (int tc = 0; tc < T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+            long k = A / B;
+            sb.append(k * k).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
