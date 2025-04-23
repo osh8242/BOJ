b908c8d9955253bd8ba3c12a7b8a7a1fb719842c
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-04-24 06:24:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 428 ms, Memory: 105428 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,25 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int min = 1000000;
-		int max = -1000000;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-			if (temp > max) {
-				max = temp;
-			}
-			if (temp < min) {
-				min = temp;
-			}
-		}
-		System.out.printf("%d %d", min, max);
-
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        while (st.hasMoreTokens()) {
+            int v = Integer.parseInt(st.nextToken());
+            if (v < min) min = v;
+            if (v > max) max = v;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(min).append(' ').append(max);
+        System.out.println(sb.toString());
+    }
 }
