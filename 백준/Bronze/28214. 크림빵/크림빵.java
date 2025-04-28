12a61fb2562962cac21526bdb5a6106343de156e
osh8242 <127957174+osh8242@users.noreply.github.com>
2025-04-29 06:43:18 +0900

    [Bronze III] Title: 크림빵, Time: 68 ms, Memory: 11864 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.java
+++ 백준/Bronze/28214. 크림빵/크림빵.java
@@ -1,31 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            int p = 0;
-            boolean flag = true;
-            for (int j = 1; j <= K; j++) {
-                int c = Integer.parseInt(st.nextToken());
-                if (c == 0) p++;
-                if (p == P) {
-                    flag = false;
-                    break;
+        int sellable = 0;
+        for (int bundle = 0; bundle < N; bundle++) {
+            int noCreamCount = 0;
+            for (int i = 0; i < K; i++) {
+                if (Integer.parseInt(st.nextToken()) == 0) {
+                    noCreamCount++;
                 }
-                if (K - j < P - p) break;
             }
-            if (flag) count++;
+            if (noCreamCount < P) {
+                sellable++;
+            }
         }
-        System.out.println(count);
+        System.out.println(sellable);
     }
 }
