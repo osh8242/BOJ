60def7faeb881818b05f4b67076db78b5bbf5e70
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-15 21:56:53 +0900

    [level 0] Title: 코드 처리하기, Time: 3.01 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -4,19 +4,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0; i < code.length(); i++) {
             char ch = code.charAt(i);
-            switch(mode) {
-                case 0:
-                    if (ch == '1') mode = 1;
-                    else if (isEven(i)) sb.append(ch);
-                    break;
-                case 1:
-                    if (ch == '1') mode = 0;
-                    else if (!isEven(i)) sb.append(ch);
-                    break;
-                default:
-                    break;
-            }
-                
+            if (ch == '1') mode = mode == 0 ? 1 : 0;
+            else if (i % 2 == mode) sb.append(ch);
         }
         if(sb.length() == 0) return "EMPTY";
         return sb.toString();
