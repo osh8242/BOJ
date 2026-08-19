ac948878273c2894dcb3d82d97731d95f0a7b041
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-19 23:22:02 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        return a==b && b==c ? (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c) :
+        a != b && b != c && c != a ? (a+b+c) :         (a+b+c)*(a*a+b*b+c*c);
+    }
+}
