e12a1782a6a8a94edee0c08228a245da215cdac1
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-23 23:35:17 +0900

    [level 0] Title: 세로 읽기, Time: 8.66 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        return IntStream.range(0, my_string.length())
+            .filter(i -> i % m == c - 1)                
+            .mapToObj(i -> String.valueOf((char) my_string.charAt(i)))
+            .collect(Collectors.joining());
+    }
+}
