24c3ef73782bc5d40dc0b19de14d67fcca458068
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-23 18:58:03 +0900

    [level 0] Title: 배열 만들기 5, Time: 18.01 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {        
+        return Arrays.stream(intStrs)
+            .map(str -> str.substring(s, s+l))
+            .mapToInt(Integer::parseInt)
+            .filter(v -> v > k)
+            .toArray();        
+    }
+}
