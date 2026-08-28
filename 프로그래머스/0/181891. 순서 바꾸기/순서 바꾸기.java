9e2ed5c69fb5a6a93d86b302f2b84fa304879cb3
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-28 22:57:20 +0900

    [level 0] Title: 순서 바꾸기, Time: 8.50 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.concat(
+            IntStream.range(n, num_list.length)
+                .map(i -> num_list[i]),                
+            IntStream.range(0, n)
+                .map(i -> num_list[i])                
+        ).toArray();
+    }
+}
