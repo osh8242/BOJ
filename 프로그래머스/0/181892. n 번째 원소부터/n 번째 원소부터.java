fecb9389ef222552b661123cceb754a9e14169b5
osh8242 <127957174+osh8242@users.noreply.github.com>
2026-08-28 22:50:56 +0900

    [level 0] Title: n 번째 원소부터, Time: 3.74 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.range(n-1, num_list.length)
+            .map(idx -> num_list[idx])
+            .toArray();
+    }
+}
